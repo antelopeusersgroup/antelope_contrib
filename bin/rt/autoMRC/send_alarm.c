5ecfd8da365c6356a6f8fa4b4d5e72dd94ca1bc2
Marina Glushko <anza@ucsd.edu>
1999-03-31 21:19:50 +0000

    add missing file; 	create mail message and send alarm message

---
+++ bin/rt/autoMRC/send_alarm.c
@@ -0,0 +1,42 @@
+/************************************************************************
+  *
+  *  
+  *
+***********************************************************************/
+#include "mrc.h"
+
+extern char *new_name();
+
+send_alarm(int mrcnum, int dasid, char *who )
+
+{
+
+  int 	nadd, i;
+  char    alarm_str[512],
+          key[8], mail_str[512], 
+	  *madd, *das ;
+   
+  
+  nadd = maxtbl(MailAdd);
+  if( nadd <= 0 ) 
+     die( 0, "can't get a mail recipients addresses\n");
+ 
+  sprintf( key, "%d\0", dasid);
+  if( ( das = getarr( MailSent, key)) == 0 )  {
+       das = (char *) new_name( key );
+       setarr( MailSent, key, das );
+				                 
+
+       sprintf( alarm_str, 
+           "WARNING %d MRC commands was sent to %d.\nFailed to do \'mass re-centering\'.\n", 
+            mrcnum, dasid) ;
+       for( i = 0; i < nadd; i++ )  {
+           madd = (char *) gettbl( MailAdd, i );
+           sprintf( mail_str, "echo \"%s\" | mailx -v -s \"%s:MRC FAILED\" %s\0", alarm_str, key, madd );
+
+           system( mail_str );
+       }
+   } 
+
+}
+
