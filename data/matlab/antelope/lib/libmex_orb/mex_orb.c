65d3a55cb960bdb0df38c65cf19f0e18789c5d62
Kent Lindquist <kent@lindquistconsulting.com>
2000-07-26 09:17:06 +0000

    Add a library for management of dynamic-linking to mex files of orb functions

---
+++ data/matlab/antelope/lib/libmex_orb/mex_orb.c
@@ -0,0 +1,86 @@
+/*
+ * Matlab interface to Datascope package
+ *
+ * Kent Lindquist
+ * Geophysical Institute
+ * University of Alaska, Fairbanks
+ * 1999
+ */
+
+#include <stdio.h>
+#include "mex_orb.h"
+
+int
+mex_orbopen( char *filename, char *permissions )
+{
+	return orbopen( filename, permissions );
+}
+
+int
+mex_orbclose( int orbfd )
+{
+	return orbclose( orbfd );
+}
+
+int
+mex_orbtell( int orbfd )
+{
+	return orbtell( orbfd );
+}
+
+int
+mex_orbseek( int orbfd, int which )
+{
+	return orbseek( orbfd, which );
+}
+
+int
+mex_orbafter( int orbfd, double time )
+{
+	return orbafter( orbfd, time );
+}
+
+int
+mex_orbselect( int orbfd, char *regex )
+{
+	return orbselect( orbfd, regex );
+}
+
+int
+mex_orbreject( int orbfd, char *regex )
+{
+	return orbreject( orbfd, regex );
+}
+
+int	
+mex_orbping( int orbfd, int *version )
+{
+	return orbping( orbfd, version );
+}
+
+int
+mex_orbget( int orbfd, int code, int *pktid, char *srcname, double *time,
+		char **packet, int *nbytes, int *bufsize )
+{
+
+	return orbget( (int) orbfd, code, pktid, srcname, time,
+			     packet, nbytes, bufsize );
+}
+
+int
+mex_orbreap( int orbfd, int *pktid, char *srcname, double *time,
+		char **packet, int *nbytes, int *bufsize )
+{
+
+	return orbreap( (int) orbfd, pktid, srcname, time,
+			     packet, nbytes, bufsize );
+}
+
+int
+mex_orbreap_nd( int orbfd, int *pktid, char *srcname, double *time,
+		char **packet, int *nbytes, int *bufsize )
+{
+
+	return orbreap_nd( (int) orbfd, pktid, srcname, time,
+			     packet, nbytes, bufsize );
+}
---
+++ data/matlab/antelope/lib/libmex_orb/mex_orb.h
@@ -0,0 +1,32 @@
+/*
+ * Matlab interface to Datascope package
+ *
+ * Kent Lindquist
+ * Geophysical Institute
+ * University of Alaska, Fairbanks
+ * 1999
+ */
+
+#include <mex.h>
+#include <stdlib.h>
+#include "stock.h"
+#include "orb.h"
+#include "Pkt.h"
+
+#if __STDC__
+#define PL_(x) x
+#else
+#define PL_(x) ( )
+#endif /* __STDC__ */
+
+extern int mex_orbopen PL_(( char *, char * ));
+extern int mex_orbclose PL_(( int ));
+extern int mex_orbtell PL_(( int ));
+extern int mex_orbseek PL_(( int, int ));
+extern int mex_orbafter PL_(( int, double ));
+extern int mex_orbselect PL_(( int, char * ));
+extern int mex_orbreject PL_(( int, char * ));
+extern int mex_orbping PL_(( int, int * ));
+extern int mex_orbget PL_(( int, int, int *, char *, double *, char **, int *, int *  ));
+extern int mex_orbreap PL_(( int, int *, char *, double *, char **, int *, int *  ));
+extern int mex_orbreap_nd PL_(( int, int *, char *, double *, char **, int *, int *  ));
