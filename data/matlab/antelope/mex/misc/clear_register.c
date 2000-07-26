bc9fb71621c329c46009279e4e30bdea9133d8eb
Kent Lindquist <kent@lindquistconsulting.com>
2000-07-26 09:27:01 +0000

    Add miscellaneous utilities that don't fit elsewhere (yes, pffiles needs to be outside of the dbpf directory! pffiles does not take a dbpf-class instance, and Matlab uses directory structure to help delineate functions on a class)

---
+++ data/matlab/antelope/mex/misc/clear_register.c
@@ -0,0 +1,60 @@
+/* 
+ * Matlab interface to Datascope package
+ *
+ * Kent Lindquist
+ * Geophysical Institute
+ * University of Alaska, Fairbanks
+ * 1997
+ */
+
+#define USAGE "Error using ==> clear_register\n\n\
+Usage: CLEAR_REGISTER ( [ 'print' ] )\n"
+
+#include "antelope_mex.h"
+
+void mexFunction ( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] )
+{
+	int	type;
+	char	*string;
+	int	printflag;
+	double	*doublep;
+	int	*intp;
+	int	i;
+	char	**messages;
+	int	*count;
+	int	NMAX;
+	int	next, wrapflag;
+	char	s[STRSZ];
+
+	if( nrhs > 1 )
+	{
+		antelope_mexUsageMsgTxt ( USAGE );
+		return;
+	}
+
+	if( nrhs == 1 )
+	{
+		if( ! get_string( prhs[0], &string ) )
+		{
+			antelope_mexUsageMsgTxt ( USAGE );
+			return;
+		} 
+		else if( ! STREQ( string, "print" ) )
+		{
+			antelope_mexUsageMsgTxt ( USAGE );
+			return;
+		} 
+		else
+		{
+			mxFree( string );
+			printflag = 1;
+		}
+
+	}
+	else
+	{
+		printflag = 0;
+	}
+
+	antelope_mex_clear_register ( printflag );
+}
---
+++ data/matlab/antelope/mex/misc/pffiles.c
@@ -0,0 +1,76 @@
+/* 
+ * Matlab interface to Datascope package
+ *
+ * Kent Lindquist
+ * Geophysical Institute
+ * University of Alaska, Fairbanks
+ * 1999
+ */
+
+#define USAGE "Error using ==> pffiles\n\n\
+Usage: FILENAMES = PFFILES ( PFNAME [, 'all' ] )\n"
+
+#include <stdio.h>
+#include "antelope_mex.h"
+
+void mexFunction ( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] )
+{
+	char	*pfname;
+	char	*optall;
+	Tbl	*files;
+	int	all = 0;
+
+	if( nlhs > 1 ) 
+	{
+		antelope_mexUsageMsgTxt ( USAGE );
+		return;
+	}
+
+	if( nrhs == 0 || nrhs > 2 )
+	{
+		antelope_mexUsageMsgTxt ( USAGE );
+		return;
+	}
+	else if( ! get_string( prhs[0], &pfname ) )
+	{
+		antelope_mexUsageMsgTxt ( USAGE );
+		return;
+	}
+        else if( nrhs == 2 && ! get_string( prhs[1], &optall ) )
+        {
+		mxFree( pfname );
+                antelope_mexUsageMsgTxt ( USAGE );
+		return;
+        }
+	else if( nrhs == 2 )
+	{
+		if( ! STREQ( optall, "all" ) )
+		{
+			mxFree( pfname );
+			mxFree( optall );
+			antelope_mexUsageMsgTxt ( USAGE );
+			return;
+		} 
+		else
+		{
+			mxFree( optall );
+			all = 1;
+		}
+	}
+
+	files = pffiles( pfname, all );
+	antelope_mex_clear_register( 1 );
+
+	mxFree( pfname );
+
+	if( files == NULL )
+	{
+		mexErrMsgTxt( "pffiles: No relevant files found" );
+	} 
+	else
+	{
+		plhs[0] = stringtbl2cellstr( files );
+
+		freetbl( files, free );
+	}
+}
