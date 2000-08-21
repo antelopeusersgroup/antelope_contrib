b1105f67e13bf1516b38e1e14d0aa2e0a43beb58
Kent Lindquist <kent@lindquistconsulting.com>
2000-08-21 03:39:49 +0000

    Enhanced dbeval to handle multiple rows simultaneously

--- data/matlab/antelope/mex/db/dbeval.c
+++ data/matlab/antelope/mex/db/dbeval.c
@@ -15,12 +15,15 @@ Usage: DBEVAL ( DBPTR, EXPRESSION [,TYPE] )\n"
 void mexFunction ( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] )
 {
 	Dbptr	db;
+	int	single_row;
+	int	nrows;
 	int	code = 0;
 	int	rc = 0;
 	char	*expression;
 	char	*codestr;
 	Expression *expr;
 	Dbvalue	exresult;
+	double	*pr;
 
 	if( nrhs != 2 && nrhs != 3 )
 	{
@@ -61,47 +64,171 @@ void mexFunction ( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] )
 		mxFree( expression );
 	}
 
-	rc = dbex_eval ( db, expr, 1, &exresult ) ;
+	rc = dbquery( db, dbRECORD_COUNT, &nrows );
 	antelope_mex_clear_register( 1 );
-
-	if( rc >= 0 )
+	if( rc == dbINVALID ) 
 	{
-		switch( expr->type )
-		{
-		case dbBOOLEAN:
-		case dbINTEGER:
-		case dbYEARDAY:
-			plhs[0] = CreateDouble( (double)  exresult.i );
-			if( plhs[0] == NULL )
-			{
-				dbex_free( expr );
-				mexErrMsgTxt( 
-				   "dbeval: failed to create return value" );
-			}
-			break;
-		case dbREAL:
-		case dbTIME:
-			plhs[0] = CreateDouble( exresult.d );
-			if( plhs[0] == NULL )
-			{
-				dbex_free( expr );
-				mexErrMsgTxt( 
-				   "dbeval: failed to create return value" );
-			}
-			break;
-		case dbSTRING:
-			plhs[0] = mxCreateString( exresult.t );
-			break;
-		default:
-			dbex_free( expr );
-			mexErrMsgTxt( "Unknown type in dbeval\n" );
-			break;
-		}
-		dbex_free( expr );
+		mexErrMsgTxt( "dbgetv: query for number of records failed" );
 	}
-	else
+
+	if( db.record == dbALL ) {
+		if( nrows == 1 ) {
+			single_row = 1;
+			db.record = 0;
+		} else {
+			single_row = 0;
+		}
+	} else {
+		single_row = 1;
+	}
+
+	if( single_row ) 
 	{
-		dbex_free( expr );
-		mexErrMsgTxt( "dbeval: eval failed\n" );
+		rc = dbex_eval ( db, expr, 1, &exresult ) ;
+		antelope_mex_clear_register( 1 );
+
+		if( rc >= 0 )
+		{
+		   switch( expr->type )
+		   {
+			case dbBOOLEAN:
+			case dbINTEGER:
+			case dbYEARDAY:
+				plhs[0] = CreateDouble( (double)  exresult.i );
+				if( plhs[0] == NULL )
+				{
+					dbex_free( expr );
+					mexErrMsgTxt( 
+				   	"dbeval: failed to create return value" );
+				}
+				break;
+			case dbREAL:
+			case dbTIME:
+				plhs[0] = CreateDouble( exresult.d );
+				if( plhs[0] == NULL )
+				{
+					dbex_free( expr );
+					mexErrMsgTxt( 
+				   	"dbeval: failed to create return value" );
+				}
+				break;
+			case dbSTRING:
+				plhs[0] = mxCreateString( exresult.t );
+				break;
+			default:
+				dbex_free( expr );
+				mexErrMsgTxt( "Unknown type in dbeval\n" );
+				break;
+		   }
+		   dbex_free( expr );
+		}
+		else
+		{
+			dbex_free( expr );
+			mexErrMsgTxt( "dbeval: eval failed\n" );
+		}
+	} else {
+
+		db.record = 0;
+
+		rc = dbex_eval ( db, expr, 1, &exresult ) ;
+		antelope_mex_clear_register( 1 );
+
+		if( rc >= 0 )
+		{
+		   switch( expr->type )
+		   {
+			case dbBOOLEAN:
+			case dbINTEGER:
+			case dbYEARDAY:
+				plhs[0] = mxCreateDoubleMatrix( nrows, 1, mxREAL );
+				if( plhs[0] == NULL )
+				{
+					dbex_free( expr );
+					mexErrMsgTxt( 
+				   	"dbeval: failed to create return value" );
+				}
+
+				pr = mxGetPr( plhs[0] );
+				pr[db.record] = (double) exresult.i;
+
+				for( db.record = 1; db.record < nrows; db.record++ )
+				{
+					if( dbex_eval ( db, expr, 1, &exresult ) < 0 ) 
+					{
+						antelope_mex_clear_register( 1 );
+						dbex_free( expr );
+						mxDestroyArray( plhs[0] );
+						mexErrMsgTxt( "dbeval: eval failed\n" );
+					} else {
+						pr[db.record] = (double) exresult.i;
+					}
+				}
+
+				break;
+			case dbREAL:
+			case dbTIME:
+				plhs[0] = mxCreateDoubleMatrix( nrows, 1, mxREAL );
+				if( plhs[0] == NULL )
+				{
+					dbex_free( expr );
+					mexErrMsgTxt( 
+				   	"dbeval: failed to create return value" );
+				}
+
+				pr = mxGetPr( plhs[0] );
+				pr[db.record] = exresult.d;
+
+				for( db.record = 1; db.record < nrows; db.record++ )
+				{
+					if( dbex_eval ( db, expr, 1, &exresult ) < 0 ) 
+					{
+						antelope_mex_clear_register( 1 );
+						dbex_free( expr );
+						mxDestroyArray( plhs[0] );
+						mexErrMsgTxt( "dbeval: eval failed\n" );
+					} else {
+						pr[db.record] = (double) exresult.d;
+					}
+				}
+
+				break;
+			case dbSTRING:
+				plhs[0] = mxCreateCellMatrix( nrows, 1 );
+				if( plhs[0] == NULL )
+				{
+					dbex_free( expr );
+					mexErrMsgTxt( 
+				   	"dbeval: failed to create return value" );
+				}
+
+				mxSetCell( plhs[0], db.record, mxCreateString( exresult.t ) );
+
+				for( db.record = 1; db.record < nrows; db.record++ )
+				{
+					if( dbex_eval ( db, expr, 1, &exresult ) < 0 ) 
+					{
+						antelope_mex_clear_register( 1 );
+						dbex_free( expr );
+						mxDestroyArray( plhs[0] );
+						mexErrMsgTxt( "dbeval: eval failed\n" );
+					} else {
+						mxSetCell( plhs[0], db.record, mxCreateString( exresult.t ) );
+					}
+				}
+
+				break;
+			default:
+				dbex_free( expr );
+				mexErrMsgTxt( "Unknown type in dbeval\n" );
+				break;
+		   }
+		   dbex_free( expr );
+		}
+		else
+		{
+			dbex_free( expr );
+			mexErrMsgTxt( "dbeval: eval failed\n" );
+		}
 	}
 }
