2d27522fa06601ed772e0447ffe3f8e7e67b8791
Kent Lindquist <kent@lindquistconsulting.com>
2000-11-01 03:25:36 +0000

    Added dbextfile to interface

---
+++ data/matlab/antelope/examples/dbexample_dbextfile.xm
@@ -0,0 +1,25 @@
+dbexample_get_demodb_path;
+
+echo on
+
+db = dbopen( demodb_path,'r' );
+
+db = dblookup_table( db, 'wfdisc' );
+dbt = dblookup_table( db, 'sensor' );
+db = dbjoin( db, dbt )
+dbt = dblookup_table( db,'instrument' );
+db = dbjoin( db, dbt )
+
+% Find the external-file pathnames of the response file
+% and the waveform file for the first record in the
+% demo-database view formed above:
+
+db.record=0;
+
+dbextfile( db, 'instrument' )                    
+
+dbextfile( db, 'wfdisc' )                    
+
+dbclose( db );
+
+echo off
---
+++ data/matlab/antelope/mex/db/dbextfile.c
@@ -0,0 +1,63 @@
+/* 
+ * Matlab interface to Datascope package
+ *
+ * Kent Lindquist
+ * Geophysical Institute
+ * University of Alaska, Fairbanks
+ * 1997
+ */
+
+#define USAGE "Error using ==> dbextfile\n\n\
+Usage: [FILENAME STATUS] = DBEXTFILE ( DBPTR, TABLENAME )\n"
+
+#include "antelope_mex.h"
+
+void mexFunction ( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[] )
+{
+	Dbptr	db;
+	char	*tablename = 0;
+	char	filename[FILENAME_MAX];
+	char	status[STRSZ];
+	int	rc;
+
+	if( nrhs != 2 )
+	{
+		antelope_mexUsageMsgTxt ( USAGE );
+		return;
+	}
+        else if( ! get_dbptr( prhs[0], &db ) )
+        {
+                antelope_mexUsageMsgTxt ( USAGE );
+		return;
+        }
+	else if( ! get_string( prhs[1], &tablename ) )
+        {
+                antelope_mexUsageMsgTxt ( USAGE );
+		return;
+        }
+
+	rc = dbextfile( db, tablename, filename );
+	antelope_mex_clear_register( 1 );
+	switch( rc ) {
+	case 2:
+		sprintf( status, "file exists, is compressed, and is readable" );
+		break;
+	case 1:
+		sprintf( status, "file exists and is readable" );
+		break;
+	case 0:
+		sprintf( status, "file does not exist, but directory is writeable" );
+		break;
+	case -1:
+		sprintf( status, "file does not exist; directory is not writeable" );
+		break;
+	default:
+		sprintf( status, "file status unknown" );
+		break;
+	}
+
+	mxFree( tablename );
+
+	plhs[0] = mxCreateString( filename );
+	plhs[1] = mxCreateString( status );
+}
