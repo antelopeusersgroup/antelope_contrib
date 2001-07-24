a21e3e73c8d407f02e7e9a9d37272fda6d7e94ed
Dan Quinlan <danq@brtt.com>
2001-07-24 22:47:26 +0000

    These files correspond to ucpwish, but are instead a dynamic library version of the ucp extensions.

---
+++ lib/tclucp/ucp_init.c
@@ -0,0 +1,30 @@
+#include "tclExtend.h"
+#include "ucp_tcl.h"
+#include "tk.h"
+#include <math.h>
+
+EXTERN int main _ANSI_ARGS_((int     argc,
+                             char  **argv));
+int (*tclXDummyMainPtr)() = (int (*)()) main;
+
+
+#if defined(DOMAIN) && defined(SING)
+EXTERN int matherr _ANSI_ARGS_((struct exception *));
+int (*tclDummyMathPtr)() = (int (*)()) matherr;
+#endif
+
+int
+Ucp_Init(Tcl_Interp *interp)
+{
+    Tk_Window main;
+
+    Tcl_PkgProvide(interp, "Ucp", "3.0" ) ; 
+
+    main = Tk_MainWindow(interp);
+    Tcl_CreateCommand(interp, "dpadm", dpadmCmd, main, NULL) ; 
+    Tcl_CreateCommand(interp, "dcrt", dcrtCmd, main, NULL) ; 
+    Tcl_CreateCommand(interp, "dprt", dprtCmd, main, NULL) ; 
+    Tcl_CreateCommand(interp, "dcc", dccCmd, main, NULL) ; 
+
+    return TCL_OK;
+}
