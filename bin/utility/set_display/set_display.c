1f861ad87cd6ca9f761d5a64b2de08fe706cc0d2
Kent Lindquist <kent@lindquistconsulting.com>
2001-09-30 02:34:54 +0000

    move dbloc_set_display to the more general name set_display. Changed behavior: getting the opposing display now requires -o option. Added ability to avoid a specified display (necessary for display-bug workarounds)

---
+++ bin/utility/set_display/set_display.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+#include "elog.h"
+
+static void 
+usage()
+{
+	die( 1, "Usage: set_display [-o] [-a #]\n" );
+}
+
+int
+main( int argc, char **argv )
+{
+	Display *display;
+	char	*display_name = NULL;
+	char	*s;
+	int	this_screen; 
+	int	next_screen; 
+	int	use_screen; 
+	int	screen_count;
+	int	c;
+	int	avoid = -1;
+	int	opposing = 0;
+
+	while( ( c = getopt( argc, argv, "a:o" ) ) != -1 ) {
+		switch( c ) {
+		case 'a':
+			avoid = atoi( optarg );
+			break;
+		case 'o':
+			opposing++;
+			break;
+		case '?': 
+			usage();
+			break;
+		}
+	}
+
+	if( argc - optind > 0 ) {
+		usage();	
+	}
+
+	/* Suppress error messages about inability to connect to display */
+	fclose( stderr );
+
+	display = XOpenDisplay( display_name );
+
+	if( display == (Display *) NULL ) {
+		exit( 0 );
+	}
+
+	screen_count = ScreenCount( display );
+	this_screen = DefaultScreen( display );
+
+	next_screen = ( this_screen + 1 ) % screen_count;
+
+	if( avoid == next_screen ) {
+
+		use_screen = this_screen;
+
+	} else if( avoid == this_screen  ) {
+
+		use_screen = next_screen;
+
+	} else if( opposing ) {
+
+		use_screen = next_screen;
+
+	} else {
+
+		use_screen = this_screen;
+	}
+
+	s = strdup( DisplayString( display ) );
+	s[strlen(s)-1] = '\0';
+	sprintf( s, "%s%d", s, use_screen );
+
+	printf( "-display %s", s );
+
+	XCloseDisplay( display );
+
+	return 0;
+}
