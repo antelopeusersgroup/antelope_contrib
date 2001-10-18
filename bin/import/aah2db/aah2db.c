39f26688e4b07fcf58cccc6bde80d2e95d6f3869
Kent Lindquist <kent@lindquistconsulting.com>
2001-10-18 23:25:07 +0000

    Added header null routine, called before each file block

--- bin/import/aah2db/aah2db.c
+++ bin/import/aah2db/aah2db.c
@@ -10,6 +10,7 @@
 #include "elog.h"
 #include "ak_ahheader.h"
 
+static void get_null_ak_ahhead( ak_ahhed * );
 static void n2h_ak_ahhead( ak_ahhed * );
 static int aah_datatype_to_size( short );
 static int aah_abstime_to_epoch( struct time, double * );
@@ -118,6 +119,8 @@ main (int argc, char **argv)
 		fstat( fileno( fp ), &fpstat );
 
 		while( foff < fpstat.st_size ) {
+
+			get_null_ak_ahhead( &ah );
 
 			nread = fread( &ah.station, 1, 520, fp );
 			if( nread != 520 ) {
@@ -323,4 +326,57 @@ static int aah_abstime_to_epoch( struct time abstime, double *time )
 			abstime.sec );
 
 	return zstr2epoch( timestr, time );
+}
+
+static void
+get_null_ak_ahhead( hed )
+ak_ahhed	*hed;
+{
+	int	i;
+
+	strcpy(hed->station.code,"null");
+	strcpy(hed->station.chan,"null");
+	strcpy(hed->station.stype,"null");
+	hed->station.slat= 0.0;
+	hed->station.slon= 0.0;
+	hed->station.elev= 0.0;
+	hed->station.DS= 0.0;
+	hed->station.A0= 0.0;
+	for(i=0; i< NOCALPTS; ++i)
+	{
+		hed->station.cal[i].pole.r= 0.0;
+		hed->station.cal[i].pole.i= 0.0;
+		hed->station.cal[i].zero.r= 0.0;
+		hed->station.cal[i].zero.i= 0.0;
+	}
+
+	hed->event.lat= 0.0;
+	hed->event.lon= 0.0;
+	hed->event.dep= 0.0;
+	hed->event.ot.yr= (short)0;
+	hed->event.ot.mo= (short)0;
+	hed->event.ot.day= (short)0;
+	hed->event.ot.hr= (short)0;
+	hed->event.ot.mn= (short)0;
+	hed->event.ot.sec= 0.0;
+	strcpy(hed->event.ecomment,"null");
+
+	hed->record.type= (short)0;
+	hed->record.ndata= 0L;
+	hed->record.delta= 0.0;
+	hed->record.maxamp= 0.0;
+	hed->record.abstime.yr= (short)0;
+	hed->record.abstime.mo= (short)0;
+	hed->record.abstime.day= (short)0;
+	hed->record.abstime.hr= (short)0;
+	hed->record.abstime.mn= (short)0;
+	hed->record.abstime.sec= 0.0;
+	hed->record.rmin= 0.0;
+	strcpy(hed->record.rcomment,"null");
+	strcpy(hed->record.log,"null");
+
+	for(i=0; i< NEXTRAS; ++i)
+		hed->extra[i]= 0.0;
+
+	return;
 }
