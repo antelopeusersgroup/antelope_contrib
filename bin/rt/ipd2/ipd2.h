c2be2974d81de0f08a72a981096b6ca6d52d4fe2
Dan Quinlan <danq@brtt.com>
2002-03-01 21:16:29 +0000

    These are Marina's changes; I'm just committing them for her:
        ipd2: remove "dead" code -
           - open/read HSI card;
           - open/read char device;
           - open/read disk;

--- bin/rt/ipd2/ipd2.h
+++ bin/rt/ipd2/ipd2.h
@@ -40,15 +40,11 @@
 
 #define RTDAS_PORT 5000         /* input port; data from DAS  */
 
-#define DSK_BLK_SIZE    1024	/* PASSCAL disk block size  */
 #define IBUF_SIZE       4096
  
  /* Input Ports  */
 
 #define  IN_SOCKET	0x534f43  /* 'SOC'  */
-#define  IN_HSI		0x485349  /* 'HSI'  */
-#define  IN_CHR		0x434852  /* 'CHR'  */
-#define  IN_DISK	0x44534b  /* 'DSK'  */
 
 #define OFF_PIDA	4
 #define OFF_PIDB        6	
@@ -68,15 +64,6 @@ struct Prts  {
 };
 
   
-struct PsclDk {
-  int leod;
-  int maxblk;
-  char label[16];
-};
-
-struct PsclDk PsclDk;
-int Pblcks;
-
 int Log;
 int Ls;
 struct sockaddr_in myadd_in;           
@@ -92,13 +79,8 @@ struct sockaddr_in peer_in;
 extern void usage PL_(( void ));
 extern int main PL_(( int argc, char *argv[] ));
 extern int open_IN_ports PL_(( struct Prts *ports ));
-extern int open_disk PL_(( struct Prts *iports ));
-extern int open_chr PL_(( struct Prts *ports ));
 extern int open_socket PL_(( struct Prts *ports ));
 extern void *read_in_ports PL_(( struct Prts *ports, int hdrtype, int timeout ));
-extern int read_hsi PL_(( struct Prts *inport, unsigned char **buffer ));
-extern int read_chr PL_(( struct Prts *inport, unsigned char **buffer ));
-extern int read_disc PL_(( struct Prts *inport, unsigned char **buffer ));
 extern int read_socket PL_(( struct Prts *inport, int hdrtype , int timeout));
 extern int send2orb PL_ ((int *orb, char *orbname, char *packet, char *srcname, double epoch, int size, int err));
 extern int valid_pkt PL_(( unsigned char **data, char *srcname, double *epoch, int *psize, int length, int hdrtype));
