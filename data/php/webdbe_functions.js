e90f7bde19a9173d99369737fa37c696268b7f14
Real Time User <rt@epicenter.ucsd.edu>
2004-02-05 03:01:11 +0000

    Rob's intermediate version with clickable rows and tabbed pages

---
+++ data/php/webdbe_functions.js
@@ -0,0 +1,9 @@
+<!--
+function highlight(element) {
+	if(element.parentNode.parentNode.style.backgroundColor != '#FFFFCC') {
+		element.parentNode.parentNode.style.backgroundColor = '#FFFFCC';
+	} else {
+		element.parentNode.parentNode.style.backgroundColor = 'white';
+	}
+}
+//-->
