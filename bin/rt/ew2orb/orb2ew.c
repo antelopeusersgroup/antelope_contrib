a1b54fc8ea6fdfb1ea19d246cb57f9a858014bda
Kent Lindquist <kent@lindquistconsulting.com>
2004-03-23 08:06:49 +0000

    Remove some scaffolding

--- bin/rt/ew2orb/orb2ew.c
+++ bin/rt/ew2orb/orb2ew.c
@@ -11,8 +11,8 @@
 
 #include "orbew.h"
 
-#define SCAFFOLD_UNSTUFFPKT_DATATYPE "s4" 
-#define SCAFFOLD_MESS_DATASIZE 4
+#define DATATYPE "s4" 
+#define DATASIZE 4
 #define SOCKET_LISTEN_BACKLOG 10
 #define SERVER_RESET_ALLOWANCE_SEC 1
 
@@ -46,22 +46,35 @@ typedef struct ExportServerThread {
 	int	my_inst;	
 	int	my_mod;	
 	int	my_type_heartbeat;		
+	int	my_type_tracebuf;		
 	int	so;
 	struct sockaddr_in sin;
-	Bns	*bnsin;			
-	Bns	*bnsout;			
 	char	*buf;	
 	int 	bufsize;
 	int	nbytes;
 	enum Loglevel loglevel;
-	int	connectfail;
-	int	bindfail;
+
+	Tbl	*Export_Threads;
+
 } ExportServerThread;
 
+typedef struct ExportThread {
+
+	ExportServerThread *es;
+	thread_t thread_id;
+	int	so;
+	int	orbfd;
+	Bns	*bnsin;			
+	Bns	*bnsout;			
+
+} ExportThread;
+
 char	*Pfname = "orb2ew";
-int	Orbfd = -1;
+char	*Orbname = 0;
 Arr	*Export_Server_Threads;
 rwlock_t Export_Server_Threads_rwlock;
+Arr	*Pins;
+rwlock_t Pins_rwlock;
 
 static void
 usage() 
@@ -75,6 +88,26 @@ usage()
 	return;
 }
 
+static int 
+get_pinno( PktChannel *pktchan )
+{
+	char	srcname[STRSZ];
+	int	pinno;
+
+	sprintf( srcname, "%s_%s_%s", 
+			  pktchan->net, 
+			  pktchan->sta, 
+			  pktchan->chan );
+
+	rw_rdlock( &Pins_rwlock );
+
+	pinno = (int) getarr( Pins, srcname );
+
+	rw_unlock( &Pins_rwlock );
+
+	return pinno;
+}
+
 int
 pktchan_to_tracebuf( PktChannel *pktchan,
 		     TracePacket *tp,
@@ -83,31 +116,70 @@ pktchan_to_tracebuf( PktChannel *pktchan,
 {
 	char	*datap;
 	int	dsize_bytes;
+	double	endtime;
+	int	pinno;
+	char	*ptr;
 
-	strcpy( tp->trh.datatype, SCAFFOLD_UNSTUFFPKT_DATATYPE );
+	strcpy( tp->trh.datatype, DATATYPE );
 
-	tp->trh.pinno = 0; /* SCAFFOLD */
+	pinno = get_pinno( pktchan );
+	ptr = (char *) &tp->trh.pinno;
+	hi2mi( &pinno, &ptr, 1 );
 
 	strcpy( tp->trh.sta, pktchan->sta );
 	strcpy( tp->trh.chan, pktchan->chan );
 	strcpy( tp->trh.net, pktchan->net );
-	tp->trh.samprate = pktchan->samprate;
-	tp->trh.nsamp = pktchan->nsamp;
-	tp->trh.starttime = starttime;
-	tp->trh.endtime = ENDTIME( starttime,
-				   pktchan->samprate,
-				   pktchan->nsamp );
+
+	ptr = (char *) &tp->trh.samprate;
+	hd2md( &pktchan->samprate, &ptr, 1 );
+
+	ptr = (char *) &tp->trh.nsamp;
+	hi2mi( &pktchan->nsamp, &ptr, 1 );
+
+	ptr = (char *) &tp->trh.starttime;
+	hd2md( &starttime, &ptr, 1 );
+
+	ptr = (char *) &tp->trh.endtime;
+	endtime = ENDTIME( starttime, pktchan->samprate, pktchan->nsamp );
+	hd2md( &endtime, &ptr, 1 );
+
 	strcpy( tp->trh.quality, "" );
 	strcpy( tp->trh.pad, "" );
 
 	datap = &tp->msg[0] + sizeof( TRACE_HEADER );
-	dsize_bytes = SCAFFOLD_MESS_DATASIZE * tp->trh.nsamp;
+	dsize_bytes = DATASIZE * tp->trh.nsamp;
 
 	*nbytes = dsize_bytes + sizeof( TRACE_HEADER );
 
-	memcpy( datap, pktchan->data, dsize_bytes );
+	hi2mi( pktchan->data, &datap, tp->trh.nsamp );
 
 	return 0;
+}
+
+static ExportThread *
+new_ExportThread( ExportServerThread *es, int so )
+{
+	ExportThread *et;
+
+	allot( ExportThread *, et, 1 );
+
+	et->es = es; 
+	et->so = so;
+	et->orbfd = -1;
+	et->bnsin = NULL;
+	et->bnsout = NULL;
+
+	return et;
+}
+
+static void
+free_ExportThread( void *arg ) 
+{
+	ExportThread *et = (ExportThread *) arg;
+
+	free( et );
+
+	return;
 }
 
 static ExportServerThread *
@@ -122,8 +194,6 @@ new_ExportServerThread( char *name )
 	es->new = 1;
 	es->stop = 0;
 	es->pf = pfnew( PFARR );
-	es->bnsin = NULL;
-	es->bnsout = NULL;
 	es->buf = 0;
 	es->bufsize = 0;
 	es->nbytes = 0;
@@ -132,8 +202,8 @@ new_ExportServerThread( char *name )
 	es->last_heartbeat_sent = 0;
 	es->last_heartbeat_received = 0;
 	es->expect_heartbeat_hook = NULL;
-	es->connectfail = 0;
-	es->bindfail = 0;
+
+	es->Export_Threads = newtbl( 0 );
 
 	mutex_init( &es->es_mutex, USYNC_THREAD, NULL );
 
@@ -154,6 +224,11 @@ free_ExportServerThread( ExportServerThread **es )
 	if( (*es)->buf ) {
 
 		free( (*es)->buf );
+	}
+
+	if( (*es)->Export_Threads ) {
+		
+		freetbl( (*es)->Export_Threads, free_ExportThread );
 	}
 
 	free( *es );
@@ -230,7 +305,7 @@ stop_export_server_thread( char *name )
 
 	if( ( es = find_export_server_thread_byname( name ) ) == 0 ) { 
 
-		complain( 1, "stop_export_server_thread: "
+		elog_complain( 1, "stop_export_server_thread: "
 			  "Couldn't find export_server thread '%s' in registry\n",
 			  name );
 		return;
@@ -238,7 +313,7 @@ stop_export_server_thread( char *name )
 
 	if( Flags.VeryVerbose ) {
 
-		complain( 0, "'%s': Request sent to stop export_server thread, "
+		elog_notify( 0, "'%s': Request sent to stop export_server thread, "
 			     "thread-id %d\n", name, es->thread_id );
 	}
 
@@ -322,25 +397,40 @@ delete_export_server_thread( ExportServerThread *es )
 	return;
 }
 
-void
-close_export_server_connection( ExportServerThread *es )
+void 
+close_export_connection( ExportThread *et )
 {
 	char	eof = 004;
 
-	bnsput( es->bnsout, &eof, BYTES, 1 );
-	bnsflush( es->bnsout );
+	bnsput( et->bnsout, &eof, BYTES, 1 );
+	bnsflush( et->bnsout );
 
-	bnsfree( es->bnsout );
+	bnsfree( et->bnsout );
 
-	/* Keep export_generic from interpreting flushed data
+	/* Keep import_generic from interpreting flushed data
 	   as a malformed heartbeat: */
 
-	bnsclr( es->bnsin );
+	bnsclr( et->bnsin );
 
-	bnsclose( es->bnsin );
+	bnsclose( et->bnsin );
 
-	es->bnsin = 0;
+	et->bnsin = 0;
 
+	return;
+}
+
+void
+close_export_server_connection( ExportServerThread *es )
+{
+	ExportThread *et;
+
+	while( ( et = shifttbl( es->Export_Threads ) ) != NULL ) {
+
+		close_export_connection( et );
+
+		thr_kill( et->thread_id, SIGKILL );
+	}
+	
 	if( es->stop == 0 ) {
 		
 		sleep( SERVER_RESET_ALLOWANCE_SEC );
@@ -358,15 +448,12 @@ orb2ew_export_server_shutdown()
 
 	if( ( es = find_export_server_thread_byid( thr_self() ) ) == NULL ) {
 
-		complain( 0, "Couldn't find thread %d in registry!\n",
+		elog_complain( 0, "Couldn't find thread %d in registry!\n",
 			  	  thr_self() );
 
 	} else {
 	
-		if( es->bnsin != NULL ) {
-
-			close_export_server_connection( es ); 
-		}
+		close_export_server_connection( es ); 
 
 		if( es->name != NULL ) {
 
@@ -376,7 +463,7 @@ orb2ew_export_server_shutdown()
 
 	if( Flags.verbose ) {
 
-		complain( 0, 
+		elog_notify( 0, 
 		"'%s': Thread (thread-id %d) stopping at user request\n",
 		  name, thr_self() );
 	}
@@ -406,13 +493,13 @@ reconfig_export_server_thread( ExportServerThread *es )
 
 			if( es->new ) {
 					
-				complain( 0, 
+				elog_notify( 0, 
 			  	"'%s': Configuring thread with: ", 
 			  	es->name );
 
 			} else {
 
-				complain( 0, 
+				elog_notify( 0, 
 			  	"'%s': Reconfiguring thread with: ", 
 			  	es->name );
 
@@ -421,12 +508,7 @@ reconfig_export_server_thread( ExportServerThread *es )
 			pfout( stderr, es->pf );
 		}
 			
-		if( es->bnsin != NULL ) {
-			
-			close_export_server_connection( es ); 
-
-			es->last_heartbeat_sent = 0;
-		}
+		close_export_server_connection( es ); 
 
 		strcpy( es->server_ipaddress,
 			pfget_string( es->pf, "server_ipaddress" ) );
@@ -461,14 +543,19 @@ reconfig_export_server_thread( ExportServerThread *es )
 			       &es->my_mod,
 			       &es->my_type_heartbeat );
 
+		ewlogo_tologo( es->my_inst_str, 
+			       es->my_mod_str, 
+			       Default_TYPE_TRACEBUF, 
+			       &es->my_inst,
+			       &es->my_mod,
+			       &es->my_type_tracebuf );
+
 		loglevel = pfget_string( es->pf, "loglevel" );
 
 		es->loglevel = translate_loglevel( loglevel );
 		
 		es->update = 0;
 		es->new = 0;
-		es->connectfail = 0;
-		es->bindfail = 0;
 	}
 
 	mutex_unlock( &es->es_mutex );
@@ -476,9 +563,46 @@ reconfig_export_server_thread( ExportServerThread *es )
 	return;
 }
 
-static void
-refresh_export_server_thread( ExportServerThread *es )
+int
+buf_send( ExportThread *et, TracePacket *tp, int nbytes_tp )
 {
+	char	msg[STRSZ];
+	char	stx = STX;	
+	char	etx = ETX;	
+	char	esc = ESC;	
+	char	*cp;
+	int	i;
+
+	sprintf( msg, "%c%3d%3d%3d", 
+			stx,
+			et->es->my_inst,
+			et->es->my_mod,
+			et->es->my_type_tracebuf );
+	
+	cp = (char *) tp;
+
+	for( i = 0; i < nbytes_tp; i++ ) {
+
+		if( *cp == STX || *cp == ETX || *cp == ESC ) {
+
+			bnsput( et->bnsout, &esc, BYTES, 1 );
+			bnsput( et->bnsout, cp++, BYTES, 1 );
+
+		} else {
+
+			bnsput( et->bnsout, cp++, BYTES, 1 );
+		}
+	}
+	
+	bnsput( et->bnsout, &etx, BYTES, 1 );
+
+	return 0;
+}
+
+static void *
+orb2ew_export( void *arg )
+{
+	ExportThread *et = (ExportThread *) arg;
 	int	rc;
 	int     pktid;
 	char    srcname[STRSZ];
@@ -491,11 +615,97 @@ refresh_export_server_thread( ExportServerThread *es )
 	int	nbytes_orb = 0;
 	int	nbytes_tp = 0;
 	TracePacket tp;
+	int	status = 0;
+	int	pinno = 0;
+	char	*ptr;
 
+	if( ( et->orbfd = orbopen( Orbname, "r&" ) ) < 0 ) {
+		
+		elog_complain( 0,
+			"Failed to open orb '%s' for reading\n", Orbname );
+
+		status = -1;
+
+		thr_exit( (void *) &status );
+	}
+
+	et->bnsin = bnsnew( et->so, BNS_BUFFER_SIZE ); 
+	et->bnsout = bnsnew( et->so, BNS_BUFFER_SIZE ); 
+
+	bnsuse_sockio( et->bnsin );
+	bnsuse_sockio( et->bnsout );
+
+	if( et->es->loglevel == VERYVERBOSE ) {
+
+		et->bnsin->debug = 1;
+		et->bnsout->debug = 1;
+	}
+
+	if( et->es->send_heartbeat_sec*1000 < DEFAULT_BNS_TIMEOUT ) {
+
+		bnstimeout( et->bnsin, 
+			et->es->send_heartbeat_sec * 1000 );
+
+	} else {
+
+		bnstimeout( et->bnsin, DEFAULT_BNS_TIMEOUT );
+	}
+
+	for( ;; ) {
+
+		rc = orbreap( et->orbfd, &pktid, srcname, &mytime,
+			&rawpkt, (int *)&nbytes_orb, &bufsize );
+
+		if( rc < 0 )
+		{
+			clear_register( 1 );
+			continue;
+		}
+
+		rc = unstuffPkt( srcname, mytime, rawpkt, nbytes_orb, &Pkt );
+		if( rc == 0 )
+		{
+			elog_complain( 0, 
+				"Unstuff failure in orb2ew for %s\n",
+				srcname );
+			continue;
+		}
+
+		for( ichan = 0; ichan < Pkt->nchannels; ichan++ )
+		{
+			pktchan = gettbl( Pkt->channels, ichan );
+
+			if( pktchan_to_tracebuf( pktchan, &tp,
+						 mytime, &nbytes_tp  ) )
+			{
+				continue;
+			}
+
+			if( ( et->es->loglevel >= VERYVERBOSE ) || 
+				Flags.VeryVerbose ) {
+
+				ptr = (char *) &tp.trh.pinno;
+				mi2hi( &ptr, &pinno, 1 );
+
+				elog_notify( 0, 
+					"Got packet %s timed %s, will be pin %d\n", 
+					srcname, strtime( mytime ), pinno );
+			}
+
+			buf_send( et, &tp, nbytes_tp );
+		}
+	}
+}
+
+static void
+refresh_export_server_thread( ExportServerThread *es )
+{
+	int	rc;
 	int	aso;
 	struct sockaddr_in client;
 	socklen_t clientlen = sizeof( client );
 	int	on = 1;
+	ExportThread *et;
 
 	reconfig_export_server_thread( es );
 
@@ -524,167 +734,49 @@ refresh_export_server_thread( ExportServerThread *es )
 
 	rc = listen( es->so, SOCKET_LISTEN_BACKLOG ); 
 
-	aso = accept( es->so, (struct sockaddr *) &client, &clientlen ); 
-
-	if( aso >= 0 ) {
-		printf( "SCAFFOLD: Accepted socket fd %d\n", aso );
-	} else {
-		printf( "SCAFFOLD: Failed socket fd %d\n", aso );
-		perror( 0 );
-	}
-
 	for( ;; ) {
-		rc = orbreap( Orbfd, &pktid, srcname, &mytime,
-			&rawpkt, (int *)&nbytes_orb, &bufsize );
-		if( rc < 0 )
-		{
-			clear_register( 1 );
-			continue;
-		}
 
-		rc = unstuffPkt( srcname, mytime, rawpkt, nbytes_orb, &Pkt );
-		if( rc == 0 )
-		{
+		aso = accept( es->so, (struct sockaddr *) &client, &clientlen ); 
+		if( aso < 0 ) {
+			
 			elog_complain( 0, 
-				"Unstuff failure in orb2eworm for %s\n",
-				srcname );
-			continue;
+				"Failed socket fd %d on accept\n", aso );
+			continue;	
 		}
 
-		for( ichan = 0; ichan < Pkt->nchannels; ichan++ )
-		{
-			pktchan = gettbl( Pkt->channels, ichan );
+		if( ( es->loglevel >= VERBOSE ) || Flags.verbose ) {
 
-			if( pktchan_to_tracebuf( pktchan, &tp,
-						 mytime, &nbytes_tp  ) )
-			{
-				continue;
-			}
+			 elog_notify( 0, "Accepted socket fd %d\n", aso );
+		}
+		
+		et = new_ExportThread( es, aso );
 
-			fprintf( stderr, 
-				"SCAFFOLD: got %s timed %s\n", 
-				srcname, strtime( mytime ) );
+		rc = thr_create( NULL, 0, orb2ew_export, 
+				  (void *) et, 
+				  THR_DETACHED|THR_SUSPENDED,
+				  &et->thread_id );
+
+		if( rc != 0 ) {
+
+			elog_complain( 1,
+			    "'%s': Failed to create export thread: "
+			    "thr_create error %d\n", es->name, rc );
+			
+			free_ExportThread( &et );
+
+			continue;
+
+		} else {
+
+			mutex_lock( &es->es_mutex );
+
+			pushtbl( es->Export_Threads, et );
+		
+			mutex_unlock( &es->es_mutex );
+
+			thr_continue( et->thread_id );
 		}
 	}
-
-	while( es->bnsin == NULL ) {
-
-		reconfig_export_server_thread( es );
-
-		es->so = socket( PF_INET, SOCK_STREAM, 0 );
-		if( es->so < 0 ) {
-
-			complain( 1,
-		 	  "'%s': Failed to open socket to %s:%d\n", 
-			  es->name, es->server_ipaddress, es->server_port );
-
-			sleep( CONNECT_FAILURE_SLEEPTIME_SEC );
-
-			continue;
-		}
-	
-		es->sin.sin_family = AF_INET;
-		es->sin.sin_port = htons( 0 ); /* SCAFFOLD Any port */
-		es->sin.sin_addr.s_addr = htonl( INADDR_ANY );
-
-		if( bind( es->so, (struct sockaddr *) &es->sin, 
-		                                   sizeof( es->sin ) ) ) {
-			if( es->bindfail < NCOMPLAIN_MAX ) {
-
-				complain( 1, "'%s': Couldn't bind socket\n",
-					  es->name );
-			}
-
-			if( es->bindfail++ == NCOMPLAIN_MAX ) {
-					
-				complain( 0, 
-					"'%s': Last message repeated %d "
-					"times; will keep retrying "
-					"every %d sec\n",
-					es->name,
-					NCOMPLAIN_MAX,
-					CONNECT_FAILURE_SLEEPTIME_SEC );
-			}
-
-			close( es->so );
-				
-			sleep( CONNECT_FAILURE_SLEEPTIME_SEC );
-
-			continue;
-
-		} else {
-
-			es->bindfail = 0;
-		}
-	
-		es->sin.sin_port = htons( es->server_port );
-		es->sin.sin_addr.s_addr = inet_addr( es->server_ipaddress );
-
-		if( connect( es->so, (struct sockaddr *) &es->sin, sizeof( es->sin ) ) ) {
-			if( es->connectfail < NCOMPLAIN_MAX ) {
-
-				complain( 1, 
-					"'%s': Failed to connect socket for %s:%d\n",
-					es->name,
-					es->server_ipaddress, es->server_port );
-			}
-
-			if( es->connectfail++ == NCOMPLAIN_MAX ) {
-					
-				complain( 0, 
-					"'%s': Last message repeated %d "
-					"times; will keep retrying "
-					"every %d sec\n",
-					es->name,
-					NCOMPLAIN_MAX,
-					CONNECT_FAILURE_SLEEPTIME_SEC );
-			}
-
-			close( es->so );
-				
-			sleep( CONNECT_FAILURE_SLEEPTIME_SEC );
-
-			continue;
-
-		} else {
-
-			es->connectfail = 0;
-
-			if( ( es->loglevel >= VERBOSE ) || Flags.verbose ) {
-
-				complain( 0, 
-					  "'%s': import thread Connected "
-					  "to remote export module\n",
-					  es->name );
-			}
-		}
-
-		es->bnsin = bnsnew( es->so, BNS_BUFFER_SIZE ); 
-		es->bnsout = bnsnew( es->so, BNS_BUFFER_SIZE ); 
-
-		bnsuse_sockio( es->bnsin );
-		bnsuse_sockio( es->bnsout );
-#if 0
-/* SCAFFOLD */
-		if( es->loglevel == VERYVERBOSE ) {
-
-			es->bnsin->debug = 1;
-			es->bnsout->debug = 1;
-		}
-#endif
-
-		if( es->send_heartbeat_sec*1000 < DEFAULT_BNS_TIMEOUT ) {
-
-			bnstimeout( es->bnsin, 
-				es->send_heartbeat_sec * 1000 );
-
-		} else {
-
-			bnstimeout( es->bnsin, DEFAULT_BNS_TIMEOUT );
-		}
-	}
-
-	return;
 }
 
 static void *
@@ -699,14 +791,14 @@ orb2ew_export_server( void *arg )
 
 	if( Flags.verbose ) {
 
-		complain( 0,
+		elog_notify( 0,
 			  "'%s':...orb2ew_export_server thread started\n",
 			  name );
 	}
 
 	if( ( es = find_export_server_thread_byid( thr_self() ) ) == NULL ) {
 
-		complain( 1, 
+		elog_complain( 1, 
 			"Couldn't find thread id %d in registry!\n",
 			 thr_self() );
 
@@ -718,21 +810,7 @@ orb2ew_export_server( void *arg )
 	for( ;; ) {
 
 		refresh_export_server_thread( es );
-
-/* SCAFFOLD
-		send_heartbeat( it );
-
-		if( ( rc = more_packet_data( it ) ) > 0 ) {
-
-			buf_intake( it );
-
-		} else if( rc < 0 ) {
-
-			close_export_server_connection( it );
-		}
-SCAFFOLD */
 	}
-	return NULL;
 }
 
 static void
@@ -844,7 +922,7 @@ update_export_server_thread( char *name, Pf *pf )
 
 		if( ret != 0 ) {
 
-			complain( 1,
+			elog_complain( 1,
 			    "'%s': Failed to create export_server thread: "
 			    "thr_create error %d\n", name, ret );
 			
@@ -879,26 +957,78 @@ update_export_server_thread( char *name, Pf *pf )
 
 		if( es->new ) {
 
-			complain( 0,
+			elog_notify( 0,
 				"'%s': Started export_server thread as thread-id %d\n", 
 				es->name, es->thread_id );
 
 		} else if( es->update ) {
 
-			complain( 0,
+			elog_notify( 0,
 				"'%s': Posted updates for export_server thread "
 				"(thread-id %d)\n", 
 				es->name, es->thread_id );
 
 		} else {
 
-			complain( 0,
+			elog_notify( 0,
 				"'%s': ExportServerThread thread (thread-id %d) unchanged\n",
 				es->name, es->thread_id );
 		}
 	}
 
 	mutex_unlock( &es->es_mutex );
+
+	return;
+}
+
+static void 
+refresh_pins_list( Pf *pf )
+{
+	Pf	*pfpins;
+	Tbl	*new_keys;
+	char	*akey;
+	int	pinno;
+	int	i;
+
+	rw_wrlock( &Pins_rwlock );
+
+	freearr( Pins, 0 );
+
+	Pins = newarr( 0 );
+
+	if( pfget( pf, "pins", (void **) &pfpins ) != PFARR ) {
+
+		if( Flags.verbose ) {
+
+			elog_notify( 0, "No 'pins' translations array in "
+					"parameter file\n" );
+		}
+
+		rw_unlock( &Pins_rwlock );
+
+		return;
+	}
+
+	new_keys = pfkeys( pfpins );
+
+	for( i = 0; i < maxtbl( new_keys ); i++ ) {
+
+		akey = gettbl( new_keys, i );
+
+		pinno = pfget_int( pfpins, akey );
+
+		if( Flags.VeryVerbose ) {
+			
+			elog_notify( 0, "\tAssigning %s to pin %d\n",
+					akey, pinno );
+		}
+
+		setarr( Pins, akey, (void *) pinno );
+	}
+
+	freetbl( new_keys, 0 );
+
+	rw_unlock( &Pins_rwlock );
 
 	return;
 }
@@ -915,14 +1045,14 @@ reconfigure_export_server_threads( Pf *pf )
 
 	if( pfget( pf, "export_servers", (void **) &pfexport_servers ) != PFARR ) {
 
-		complain( 1, 
+		elog_complain( 1, 
 		   "parameter 'export_servers' not present or not an array\n" );
 
 		stop_all_export_server_threads();
 		
 		if( Flags.verbose && num_export_server_threads() <= 0 ) {
 
-			complain( 0, 
+			elog_complain( 0, 
 			"Warning: no export_server threads defined; nothing to do\n" );
 		}
 
@@ -937,7 +1067,7 @@ reconfigure_export_server_threads( Pf *pf )
 		
 		if( Flags.verbose && num_export_server_threads() <= 0 ) {
 
-			complain( 0, 
+			elog_complain( 0, 
 			"Warning: no export_server threads defined; nothing to do\n" );
 		}
 
@@ -980,7 +1110,7 @@ reconfigure_export_server_threads( Pf *pf )
 
 	if( Flags.verbose && num_export_server_threads() <= 0 ) {
 
-		complain( 0, 
+		elog_complain( 0, 
 		"Warning: no export_server threads defined; nothing to do\n" );
 	}
 
@@ -1001,6 +1131,10 @@ orb2ew_pfwatch( void *arg )
 
 	Export_Server_Threads = newarr( 0 );
 
+	rwlock_init( &Pins_rwlock, USYNC_THREAD, NULL );
+
+	Pins = newarr( 0 );
+
 	memset( &Ewinfo, '\0', sizeof( Earthworm_Info ) );
 
 	mutex_init( &Ewinfo.ew_mutex, USYNC_THREAD, NULL );
@@ -1015,7 +1149,7 @@ orb2ew_pfwatch( void *arg )
 
 		if( rc < 0 ) {
 			
-			complain( 1, "pfupdate pf parameter file '%s' failed\n",
+			elog_complain( 1, "pfupdate pf parameter file '%s' failed\n",
 				  Pfname );
 
 		} else if( rc == 0 ) {
@@ -1028,24 +1162,23 @@ orb2ew_pfwatch( void *arg )
 
 			if( Flags.verbose ) {
 				
-				complain( 0, 
+				elog_notify( 0, 
 				"Reconfiguring orb2ew from parameter file\n" );
 			}
+			
+			refresh_pins_list( pf );
 
 			reconfigure_export_server_threads( pf );
 		}
 
 		sleep( PFWATCH_SLEEPTIME_SEC );
 	}
-
-	return NULL;
 }
 
 int
 main( int argc, char **argv )
 {
 	char	c;
-	char	*orbname = 0;
 	int	rc;
 	thread_t pfwatch_tid;
 
@@ -1072,12 +1205,7 @@ main( int argc, char **argv )
 
 	} else {
 		
-		orbname = argv[optind++];
-	}
-
-	if( ( Orbfd = orbopen( orbname, "r&" ) ) < 0 ) {
-		
-		die( 0, "Failed to open orb '%s' for writing\n", orbname );
+		Orbname = argv[optind++];
 	}
 
 	rc = thr_create( NULL, 0, orb2ew_pfwatch, 0, 0, &pfwatch_tid );
@@ -1092,7 +1220,7 @@ main( int argc, char **argv )
 
 	if( Flags.verbose ) {
 
-		complain( 0, "Program terminated\n" );
+		elog_notify( 0, "Program terminated\n" );
 	}
 
 	return( 0 );
