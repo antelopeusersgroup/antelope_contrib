cdbff54da001b0d21ecb6e0de9238f8e7cf6323d
Gary Pavlis <pavlis@indiana.edu>
2004-07-14 14:10:02 +0000

    On cross-checking Kennett's paper I discovered I had two terms in the free surface transformation matrix reversed due to differences in coordinate systems.  At the same time I added an exception that is thrown when the algorithm is requested to compute the transformation for an evanescent wave.  (i.e. apparent slowness greater than the slowness of the medium at the surface).  The form of the transformation matrix used in this algorithm is incorrect for evanescent waves.  Kennett shows how to do this, but I've not implemented it (Hilbert transform is required).

--- lib/seismic/libseispp/freetran.cc
+++ lib/seismic/libseispp/freetran.cc
@@ -16,11 +16,21 @@ translation of m file from Michael Bostock.
 Author:  Gary Pavlis
 */
 void Three_Component_Seismogram::free_surface_transformation(Slowness_vector uvec,
-		 double a0, double b0)
+		 double a0, double b0) 
 {
+	double a02,b02,pslow,p2;
+	double qa,qb,vpz,vpr,vsr,vsz;
+	double umag=uvec.mag();
+	pslow=uvec.mag();
 	// silently do nothing if magnitude of the slowness vector is 0
 	// (vertical incidence)
-	if(uvec.mag()<DBL_EPSILON) return;
+	if(pslow<DBL_EPSILON) return;
+	// Can't handle evanescent waves with this operator
+	double vapparent=1.0/pslow;
+	if(vapparent<a0 || vapparent<b0)
+		throw seispp_error(string("free_surface_transformation:  ")
+		+string("cannot handle evanescent waves.\n")
+		+string("Surface velocities must be smaller than apparent velocity"));
 
 	// First the horizonal rotation
 	Spherical_Coordinate scor;
@@ -31,11 +41,8 @@ void Three_Component_Seismogram::free_surface_transformation(Slowness_vector uve
 	// x2=radial horizonal, and x3 is still vertical
 	this->rotate(scor);
 
-	double a02,b02,pslow,p2;
-	double qa,qb,vpz,vpr,vsr,vsz;
 	a02=a0*a0;
 	b02=b0*b0;
-	pslow=uvec.mag();
 	p2=pslow*pslow;
 	qa=sqrt((1.0/a02)-p2);
 	qb=sqrt((1.0/b02)-p2);
@@ -47,9 +54,9 @@ void Three_Component_Seismogram::free_surface_transformation(Slowness_vector uve
 	// This is different from Bostock's original code
 	// in sign and order.  
 	double fstran[3][3];
-	fstran[0][0]=1.0;  fstran[0][1]=0.0;  fstran[0][2]=0.0;
-	fstran[1][0]=0.0;  fstran[1][1]=vsr;  fstran[1][2]=vsz;
-	fstran[2][0]=0.0;  fstran[2][1]=vpr;  fstran[2][2]=vpz;
+	fstran[0][0]=0.5;  fstran[0][1]=0.0;  fstran[0][2]=0.0;
+	fstran[1][0]=0.0;  fstran[1][1]=vsr;  fstran[1][2]=vpr;
+	fstran[2][0]=0.0;  fstran[2][1]=vsz;  fstran[2][2]=vpz;
 	this->apply_transformation_matrix(fstran);
 
 	components_are_cardinal=false;
