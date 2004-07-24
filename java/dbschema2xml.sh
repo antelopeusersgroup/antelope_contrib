f537ade9ba378d2596c357d873cf382c68066ca2
Tobin Fricke <tfricke@epicenter.ucsd.edu>
2004-07-24 06:25:08 +0000

    Small sanity checks.

--- java/dbschema2xml.sh
+++ java/dbschema2xml.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+if [ $# != 1 ]; then
+  echo Usage: $0 [schema file]
+  exit
+fi
+if [ ! -e $1 ]; then
+  echo $0: error: "$1" does not exist. 
+  echo You have to specify the complete filename to the schema definition file.
+  exit
+fi
 exec java -cp $ANTELOPE/data/java/antelope.jar com.brtt.antelope.DatabaseSchema $1 -toXML
