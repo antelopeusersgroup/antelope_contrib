bfcaf1a4650c283642b6d15e886eeb0003e840c0
Dan Quinlan <danq@brtt.com>
2004-10-12 16:52:41 +0000

    rename Makefile so that this works properly with Darwin's "case insensitive" filesystem

--- lib/perl/perlpolygon/runmake.sh
+++ lib/perl/perlpolygon/runmake.sh
@@ -7,14 +7,17 @@ if ( @ARGV > 1 )
 
 delete $ENV{'KEEP_STATE'} ; 
 
+$Makefile = "perl_makefile" ;
 if ( $ARGV[0] eq "clean" ) { 
-    system ( "make -f Makefile @ARGV" ) ; 
+    system ( "make -f $Makefile CC=cc @ARGV" ) ; 
 } elsif ( $ARGV[0] eq "install" ) { 
-    system ( "make -f Makefile ; make -f Makefile install " ) ;
+    system ( "make -f $Makefile CC=cc ; make -f $Makefile CC=cc install " ) ;
 } elsif ( $ARGV[0] eq "all" ) { 
-    system ( "make -f Makefile " ) ;
+    system ( "make -f $Makefile CC=cc " ) ;
+} elsif ( $ARGV[0] eq "realclean" ) { 
+    system ( "make -f $Makefile CC=cc @ARGV" ) ; 
 } else { 
-    system ( "make -f Makefile " ) ;
+    system ( "make -f $Makefile CC=cc " ) ;
 }
 
 # $Id$ 
