cc3198fb514f82ff041f6056651e3ba3cd84fe65
Robert Newman <rlnewman@ucsd.edu>
2004-11-18 23:30:18 +0000

    Complete rewrite of webdbe to allow subset queries, new stylesheet to allow 100% customization, also insert global variables for recs_per_page

--- data/php/webdbe_functions.js
+++ data/php/webdbe_functions.js
@@ -62,4 +62,24 @@ startList = function() {
 	}
 }
 window.onload=startList;
+
+// Drop down menus for IE
+// source:http://www.htmldog.com/articles/suckerfish/dropdowns/
+sfHover = function() {
+	var sfEls = document.getElementById("menus").getElementsByTagName("LI");
+	for (var i=0; i<sfEls.length; i++) {
+		sfEls[i].onmouseover=function() {
+			this.className+=" sfhover";
+		}
+		sfEls[i].onmouseout=function() {
+			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+		}
+	}
+}
+if (window.attachEvent) window.attachEvent("onload", sfHover);
+
+function queryInput( myValue ) {
+	document.webdbeForm.webdbeQueryVal.value = myValue ;
+}
+
 //-->
