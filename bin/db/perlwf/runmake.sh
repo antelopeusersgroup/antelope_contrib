a3833621a7bde50a2660435486e02b4930cd861a
Dan Quinlan <danq@brtt.com>
2005-02-17 04:44:15 +0000

    make this safe for Darwin's case "insensitivity" so make clean doesn't remove makefile, for instance, and Makefile doesn't overwrite makefile.

--- bin/db/perlwf/runmake.sh
+++ bin/db/perlwf/runmake.sh
@@ -2,19 +2,21 @@
 eval 'exec perl -S $0 "$@"'
 if 0;
 
+$makefile = "perl_makefile" ; 
+
 if ( @ARGV > 1 )
     { die ( "Usage: $0 [all|install|clean]\n" ) ; }
 
 delete $ENV{'KEEP_STATE'} ; 
 
 if ( $ARGV[0] eq "clean" ) { 
-    system ( "make -f Makefile @ARGV" ) ; 
+    system ( "make -f $makefile @ARGV" ) ; 
 } elsif ( $ARGV[0] eq "install" ) { 
-    system ( "make -f Makefile ; make -f Makefile install " ) ;
+    system ( "make -f $makefile ; make -f $makefile install " ) ;
 } elsif ( $ARGV[0] eq "all" ) { 
-    system ( "make -f Makefile " ) ;
+    system ( "make -f $makefile " ) ;
 } else { 
-    system ( "make -f Makefile " ) ;
+    system ( "make -f $makefile " ) ;
 }
 
 # $Id$ 
