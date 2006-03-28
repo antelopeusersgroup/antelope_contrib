db0c04cbbae3ae4250749486745d3d77e4cc89a5
Dan Quinlan <danq@brtt.com>
2006-03-28 20:26:13 +0000

    changes to make just one big reference guide

--- data/refguides/refguide.sh
+++ data/refguides/refguide.sh
@@ -1,3 +1,4 @@
+#!/bin/sh -x
 
 if [ $# -ne 1 ] ; then 
     echo "Usage: refguide name"
@@ -6,7 +7,20 @@ fi
 
 name=$1
 output=$1.mif
-input=$ANTELOPE/data/doc/refguides/$name/$name.ref
+case $name in
+c|user|scripting) 
+    input=$ANTELOPE/data/doc/refguides/$name/$name.ref 
+    txform ref2mif $input > $output ;;
 
-txform ref2mif $input > $output
+combined) 
+    for input in user scripting c tables 
+    do
+	echo combined::$input
+	txform ref2mif $ANTELOPE/data/doc/refguides/$input/${input}0.ref > ${input}0.mif 
+    done ;;
+
+*) echo "Usage: refguide [c|user|scripting|combined]"
+    exit 1 ;;
+esac
+
 
