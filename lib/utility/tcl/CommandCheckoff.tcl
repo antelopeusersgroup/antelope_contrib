a88a4ddf1e27f377c474c7c729fe0e9d9983e537
Kent Lindquist <kent@lindquistconsulting.com>
2006-06-09 15:34:34 +0000

    Support for recent changes to checklist

--- lib/utility/tcl/CommandCheckoff.tcl
+++ lib/utility/tcl/CommandCheckoff.tcl
@@ -1,9 +1,13 @@
 proc CommandCheckoff {w args} { 
 	frame $w 
 	button $w.b 
+	set command_configured 0
+	set commandcolor ""
+	set nocommandcolor ""
 	checkbutton $w.cb -command "$w.b config -state normal"
 	pack $w.cb -side left -fill y 
 	pack $w.b -side left -fill x -expand 1
+
 	while { ! [lempty $args] } {
 		set option [lvarpop args]
 		switch -- $option {
@@ -24,12 +28,34 @@ proc CommandCheckoff {w args} {
 			      $w config -background $value }
 		-command { 
 			set value [lvarpop args] 
+			if { $value != "" } {
+				set command_configured 1
+			}
 			$w.b config -command \
 			     "$w.cb select; \
 			     $w.b config -state disabled; \
 			     $value"
 			}
+		-commandcolor {
+			if { $value != "" } {
+			     set commandcolor [lvarpop args] 
+			}
+			}
+		-nocommandcolor {
+			if { $value != "" } {
+			     set nocommandcolor [lvarpop args] 
+			}
+			}
 		}
 	}
+	if { $command_configured == 1 && $commandcolor != "" } {
+
+		$w.b config -activebackground $commandcolor
+
+	} elseif { $command_configured == 0 && $nocommandcolor != "" } {
+
+		$w.b config -activebackground $nocommandcolor
+	}
+
 	return $w
 }
