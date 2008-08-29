ff0940ea2d2012455bb8bbc965eb60151ff42c45
Gary Pavlis <pavlis@indiana.edu>
2008-08-29 17:03:28 +0000

    Add secondary sifting method.
    Previous version sometimes left completely redundant event clusters.
    This adds a new secondary filter to reduce redundancy.
    Man page explains the new feature.

--- bin/location/cluster_cleanup/cluster_cleanup.cc
+++ bin/location/cluster_cleanup/cluster_cleanup.cc
@@ -1,5 +1,8 @@
 #include <string>
 #include <vector>
+#include <list>
+#include <map>
+#include <set>
 #include <algorithm>
 #include <functional>
 #include "dbpp.h"
@@ -9,7 +12,6 @@
 #include "seispp.h"
 using namespace std;
 using namespace SEISPP;
-
 void usage()
 {
 	cbanner("$Revision$ $Date$",
@@ -91,6 +93,22 @@ Hypocentroid::Hypocentroid(DatascopeHandle& dbh)
 	gridlon=rad(gridlon);
 	gridz=dbh.get_double("depth");
 }
+/* local typedefs for this program */
+
+/* this is used to hold a list of evids that are associated to one gridid */
+typedef struct {
+	int gridid;
+	int nass;
+	set<int> evids;
+} EvidData;
+/* An EvidMap is used to have a keyed list of evids that are tested to remove redundant
+clusters */
+typedef map<int,EvidData> EvidMap;  // convenient typedef to avoid confusing declarations below
+/* This map is build and edited to remove redundant hypocentroids.  Survivors are written to output */
+typedef map<int,Hypocentroid> HypoMap;
+/* used to control redundancy check method.  */
+typedef enum {EXACTMATCH, SUBSETMATCH, DISTANCEONLY} CheckMethod;
+
 
 double get_cell_radius(GCLgrid3d& grid,int i, int j, int k)
 {
@@ -186,11 +204,72 @@ void dbsave(DatascopeHandle dbh, Hypocentroid h, string gridname)
 		"hcdepth",h.z,0);
 	if(ierr<0) cerr<< "dbaddv err9r saving result for gridid "<<h.gridid<<endl;
 }
-int index_to_gridid(GCLgrid3d& g, int i, int j, int k)
+/* Simple parser to convert a word into the CheckMethod enum */
+CheckMethod strtocm(string s)
 {
-	return( 1+k+j*(g.n3)+i*(g.n2)*(g.n3) );
+	CheckMethod result;
+	if(s=="exact")
+		result=EXACTMATCH;
+	else if(s=="maximum_associations")
+		result=SUBSETMATCH;
+	else if(s=="distance_only")
+		result=DISTANCEONLY;
+	else
+		throw SeisppError("strtcocm: parameter redundant_check_method value="+s
+		   + string(" is invalid\nMust be:  exact, maximum_associations, or distance_only"));
+	return(result);
 }
 
+bool is_redundant_test(EvidData& ed1, EvidData& ed2, CheckMethod rcm)
+{
+	bool result;
+	set<int>::iterator evs1e,evs2e,evsptr,evs2ptr;
+	evs1e=ed1.evids.end();
+	evs2e=ed2.evids.end();
+	switch (rcm)
+	{
+	case EXACTMATCH:
+		if(ed1.evids.size()==ed2.evids.size())
+			result=false;
+		else
+		{
+			result=true;
+			for(evsptr=ed1.evids.begin();evsptr!=evs1e;++evsptr)
+			{
+				evs2ptr=ed2.evids.find(*evsptr);
+				if(evs2ptr==evs2e)
+				{
+					result=false;
+					break;
+				}
+			}
+		}
+		break;
+	case SUBSETMATCH:
+		if(ed1.evids.size()<ed2.evids.size())
+			throw SeisppError("is_redundant_test:  second group is larger than first.  Probably coding or db error");
+		else
+		{
+			/*Since we can be sure ed2 is smaller than ed1 we just need to match 2 to 1 and stop if there
+			are any mismatches.  Note this is the reverse order of EXACTMATCH */
+			result=true;
+			for(evs2ptr=ed2.evids.begin();evs2ptr!=evs2e;++evs2ptr)
+			{
+				evsptr=ed1.evids.find(*evs2ptr);
+				if(evsptr==evs1e)
+				{
+					result=false;
+					break;
+				}
+			}
+		}
+		break;
+	case DISTANCEONLY:
+	default:
+		result= false;
+	}
+	return(result);
+}
 	
 bool SEISPP::SEISPP_verbose(false);
 int main(int argc, char **argv)
@@ -229,6 +308,8 @@ int main(int argc, char **argv)
 	try {
 		Metadata params(pf);
 		string gridname=params.get_string("gridname");
+		string redundant_check_method=params.get_string("redundant_check_method");
+		CheckMethod rcm=strtocm(redundant_check_method);
 		DatascopeHandle dbh(dbin,true);
 		DatascopeHandle dbhout(dbout,false);
 		dbh.lookup("hypocentroid");
@@ -236,19 +317,20 @@ int main(int argc, char **argv)
 		string sstring(string("gridname=~/")+gridname+string("/"));
 		dbh.subset(sstring);
 		dbh.rewind();
-		vector<Hypocentroid> hypocen;
+		vector<Hypocentroid> *hypocen=new vector<Hypocentroid>;
 		for(i=0;i<dbh.number_tuples();++dbh,++i)
 		{
 			Hypocentroid h(dbh);
-			if(h.nassoc>0) hypocen.push_back(h);
+			if(h.nassoc>0) hypocen->push_back(h);
 		}
 		cout << "Number of hypocentroids to process="
-			<< hypocen.size()<<endl;
+			<< hypocen->size()<<endl;
 
 		GCLgrid3d grid(dbh.db,gridname);
 		double cell_radius,cell_dz,zmin,zmax;
 		list<Hypocentroid> hcell;
 		vector<Hypocentroid>::iterator hptr;
+		HypoMap pass1survivors;
 		double hcdistkm;
 		int gridid;
 		for(i=0;i<grid.n1;++i)
@@ -264,7 +346,7 @@ int main(int argc, char **argv)
 				zmax=depth+cell_dz/2.0;
 				// Push hypocentroids inside this cell
 				// to list hcell
-				for(hptr=hypocen.begin();hptr!=hypocen.end();
+				for(hptr=hypocen->begin();hptr!=hypocen->end();
 					++hptr)
 				{
 					hcdistkm=deg(hptr->distance(lat,lon));
@@ -291,14 +373,104 @@ int main(int argc, char **argv)
 						hmax=max_element(hcell.begin(),
                                         	   hcell.end(),
 						   less<Hypocentroid>());
-					// Need to reset the gridid 
-					gridid=index_to_gridid(grid,i,j,k);
-					if(Verbose)cout << hmax->gridid <<"->"<<gridid;
-					//hmax->gridid=gridid;
-					dbsave(dbhout,*hmax,gridname);
+					pass1survivors.insert(pair<int,Hypocentroid>(hmax->gridid,*hmax));
 				}
 				hcell.clear();
 			}
+		cout << "First pass distance check reduced grid count from "
+			<< dbh.number_tuples()  << " to " << pass1survivors.size()<<endl;
+		if(rcm!=DISTANCEONLY)
+		{
+		cout << "Running secondary cleanup with method="<<redundant_check_method<<endl;
+		/* Pass 2 searchs for totally redundant cluster groups.  Algorithm is
+		to work through in order of number of associations.  Delete any hypocentroid 
+		for which the evid members are entirely contained in another cell with the
+		the same or more associations.  First step is to load information from a join
+		of hypocentroid and cluster.  We use this double stl container to manage this.
+		We use the database to do the sort by nass*/
+		list<string> sortkeys,grpkeys;
+		sortkeys.push_back("nass");
+		dbh.sort(sortkeys);
+		dbh.natural_join("cluster");
+		/* there is a one to many relation for hypocentroid->cluster so we don't need another sort here */
+		grpkeys.push_back("gridid");
+		dbh.group(grpkeys);
+		EvidMap emap;
+		/* load emap by looping through this dbh view */
+		dbh.rewind();
+		EvidData thisgroup;
+		/* this will contain a list of all gridids loaded in nass order.  This will be used as the pattern
+		to work through the data.  We'll selectively destroy the map contents. */
+		list<int> gridids;
+		for(i=0;i<dbh.number_tuples();++dbh,++i)
+		{
+			DBBundle dbb=dbh.get_range();
+			Dbptr parent=dbb.parent;
+			for(parent.record=dbb.start_record;parent.record<dbb.end_record;++parent.record)
+			{
+				/* a bit inefficient to constantly reset gridid and nass in thisgroup, but minor
+				compared to overhead of making the dbgetv call */
+				int thisevid;
+				dbgetv(parent,0,"gridid",&(thisgroup.gridid),
+					"evid",&thisevid,
+					"nass",&(thisgroup.nass),0);
+				thisgroup.evids.insert(thisevid);
+			}
+			emap.insert(pair<int,EvidData>(thisgroup.gridid,thisgroup));
+			/* use push front to reverse the list, avoids a confusing reverse iterator */
+			gridids.push_front(thisgroup.gridid);
+			thisgroup.evids.clear();
+		}
+		/* Now the really messy algorithm.  Work through gridid as the master list.  
+		If a gridid isn't found in both lists immediately jump to the next gridid.  
+		Otherwise enter the search or redundant lists block. */
+		list<int>::iterator grdptr,grdpass2;
+		/* use these a lot so we set them here once */
+		EvidMap::iterator emend=emap.end();
+		HypoMap::iterator hmend=pass1survivors.end();
+		EvidMap::iterator ed,ed2;
+		HypoMap::iterator hc,hc2;
+		
+		for(grdptr=gridids.begin();grdptr!=gridids.end();++grdptr)
+		{
+			gridid=*grdptr;  // recycle this symbol declared far up there
+			ed=emap.find(gridid);
+			hc=pass1survivors.find(gridid);
+			if(ed!=emend && hc!=hmend)
+			{
+				grdpass2=grdptr;
+				++grdpass2;  // set to next.  Produces odd construct in this for loop
+				for(;grdpass2!=gridids.end();++grdpass2)
+				{
+					int gridid2=*grdpass2;
+					ed2=emap.find(gridid2);
+					hc2=pass1survivors.find(gridid2);
+					if(ed2!=emend && hc2!=hmend)
+					{
+						if(is_redundant_test(ed->second,ed2->second,rcm))
+						{
+							emap.erase(gridid2);
+							pass1survivors.erase(gridid2);
+							/* I think this is necessary as these
+							are invalidated after an erase */
+							emend=emap.end();
+							hmend=pass1survivors.end();
+						}
+					}
+				}
+			}
+		}
+		}
+		/* All cases land here to save contents of pass1survivors.  In the
+		DISTANCEONLY case (only case available in older versions) this is
+		unedited.  For others his list is beaten down by more complex algorithms
+		above */
+		cout << "Writing new hypocentroid table with "<<pass1survivors.size()<<" rows"<<endl;
+		HypoMap::iterator hc;
+		for(hc=pass1survivors.begin();hc!=pass1survivors.end();++hc)
+		{
+			dbsave(dbhout,hc->second,gridname);
+		}			
 	}
 	catch (MetadataGetError mdge)
 	{
@@ -309,6 +481,11 @@ int main(int argc, char **argv)
 	{
 		sdbe.log_error();
 		exit(-2);
+	}
+	catch (SeisppError serr)
+	{
+		serr.log_error();
+		exit(-3);
 	}
 	catch (...)
 	{
