948c048f4b4cf89b578977959e15fa1275c159ff
Kent Lindquist <kent@lindquistconsulting.com>
2008-09-21 08:16:01 +0000

    Add pfget_string, pfget_double, pfget_int, pfget_time, pfget_boolean, pfget_size

--- data/python/stock/_stock.c
+++ data/python/stock/_stock.c
@@ -54,15 +54,227 @@ char *__progname = "Python";
 
 #endif
 
+static PyObject *python_pfget_string( PyObject *self, PyObject *args );
+static PyObject *python_pfget_int( PyObject *self, PyObject *args );
+static PyObject *python_pfget_double( PyObject *self, PyObject *args );
+static PyObject *python_pfget_size( PyObject *self, PyObject *args );
+static PyObject *python_pfget_boolean( PyObject *self, PyObject *args );
+static PyObject *python_pfget_time( PyObject *self, PyObject *args );
 static PyObject *python_strtime( PyObject *self, PyObject *args );
 static PyObject *python_str2epoch( PyObject *self, PyObject *args );
 PyMODINIT_FUNC init_stock( void );
 
 static struct PyMethodDef stock_methods[] = {
+	{ "_pfget_string",   	python_pfget_string,   	METH_VARARGS, "Get a string value from a parameter file" },
+	{ "_pfget_int",   	python_pfget_int,   	METH_VARARGS, "Get an integer value from a parameter file" },
+	{ "_pfget_double",   	python_pfget_double,   	METH_VARARGS, "Get a double value from a parameter file" },
+	{ "_pfget_size",   	python_pfget_size,   	METH_VARARGS, "Get a size value from a parameter file" },
+	{ "_pfget_boolean",   	python_pfget_boolean,   METH_VARARGS, "Get a boolean value from a parameter file" },
+	{ "_pfget_time",   	python_pfget_time,   	METH_VARARGS, "Get a time value from a parameter file" },
 	{ "_strtime",   	python_strtime,   	METH_VARARGS, "Compute a string representation of epoch time" },
 	{ "_str2epoch",   	python_str2epoch,   	METH_VARARGS, "Compute an epoch time from a string" },
 	{ NULL, NULL, 0, NULL }
 };
+
+static PyObject *
+python_pfget_string( PyObject *self, PyObject *args ) {
+	char	*usage = "Usage: _pfget_string( pfname, pfkey )\n";
+	char	*pfname;
+	char	*pfkey;
+	char	*pfvalue;
+	char	errmsg[STRSZ];
+	Pf	*pf;
+
+	if( ! PyArg_ParseTuple( args, "ss", &pfname, &pfkey ) ) {
+
+		PyErr_SetString( PyExc_RuntimeError, usage );
+
+		return NULL;
+	}
+
+	if( ( pf = getPf( pfname ) ) == (Pf *) NULL ) {
+		
+		sprintf( errmsg, "Failure opening parameter file '%s'\n", pfname );
+
+		PyErr_SetString( PyExc_RuntimeError, errmsg );
+
+		return NULL;
+	}
+
+	pfvalue = pfget_string( pf, pfkey );
+
+	if( pfvalue == NULL ) {
+
+		sprintf( errmsg, "Failed to find parameter '%s' in parameter file '%s'\n", pfkey, pfname );
+
+		PyErr_SetString( PyExc_RuntimeError, errmsg );
+
+		return NULL;
+	}
+
+	return Py_BuildValue( "s", pfvalue );
+}
+
+static PyObject *
+python_pfget_int( PyObject *self, PyObject *args ) {
+	char	*usage = "Usage: _pfget_string( pfname, pfkey )\n";
+	char	*pfname;
+	char	*pfkey;
+	long	pfvalue;
+	char	errmsg[STRSZ];
+	Pf	*pf;
+
+	if( ! PyArg_ParseTuple( args, "ss", &pfname, &pfkey ) ) {
+
+		PyErr_SetString( PyExc_RuntimeError, usage );
+
+		return NULL;
+	}
+
+	if( ( pf = getPf( pfname ) ) == (Pf *) NULL ) {
+		
+		sprintf( errmsg, "Failure opening parameter file '%s'\n", pfname );
+
+		PyErr_SetString( PyExc_RuntimeError, errmsg );
+
+		return NULL;
+	}
+
+	pfvalue = pfget_int( pf, pfkey );
+
+	return Py_BuildValue( "l", pfvalue );
+}
+
+static PyObject *
+python_pfget_double( PyObject *self, PyObject *args ) {
+	char	*usage = "Usage: _pfget_double( pfname, pfkey )\n";
+	char	*pfname;
+	char	*pfkey;
+	double	pfvalue;
+	char	errmsg[STRSZ];
+	Pf	*pf;
+
+	if( ! PyArg_ParseTuple( args, "ss", &pfname, &pfkey ) ) {
+
+		PyErr_SetString( PyExc_RuntimeError, usage );
+
+		return NULL;
+	}
+
+	if( ( pf = getPf( pfname ) ) == (Pf *) NULL ) {
+		
+		sprintf( errmsg, "Failure opening parameter file '%s'\n", pfname );
+
+		PyErr_SetString( PyExc_RuntimeError, errmsg );
+
+		return NULL;
+	}
+
+	pfvalue = pfget_double( pf, pfkey );
+
+	return Py_BuildValue( "d", pfvalue );
+}
+
+static PyObject *
+python_pfget_size( PyObject *self, PyObject *args ) {
+	char	*usage = "Usage: _pfget_size( pfname, pfkey )\n";
+	char	*pfname;
+	char	*pfkey;
+	double	pfvalue;
+	char	errmsg[STRSZ];
+	Pf	*pf;
+
+	if( ! PyArg_ParseTuple( args, "ss", &pfname, &pfkey ) ) {
+
+		PyErr_SetString( PyExc_RuntimeError, usage );
+
+		return NULL;
+	}
+
+	if( ( pf = getPf( pfname ) ) == (Pf *) NULL ) {
+		
+		sprintf( errmsg, "Failure opening parameter file '%s'\n", pfname );
+
+		PyErr_SetString( PyExc_RuntimeError, errmsg );
+
+		return NULL;
+	}
+
+	pfvalue = pfget_size( pf, pfkey );
+
+	return Py_BuildValue( "d", pfvalue );
+}
+
+static PyObject *
+python_pfget_boolean( PyObject *self, PyObject *args ) {
+	char	*usage = "Usage: _pfget_boolean( pfname, pfkey )\n";
+	char	*pfname;
+	char	*pfkey;
+	int	pfvalue;
+	char	errmsg[STRSZ];
+	Pf	*pf;
+
+	if( ! PyArg_ParseTuple( args, "ss", &pfname, &pfkey ) ) {
+
+		PyErr_SetString( PyExc_RuntimeError, usage );
+
+		return NULL;
+	}
+
+	if( ( pf = getPf( pfname ) ) == (Pf *) NULL ) {
+		
+		sprintf( errmsg, "Failure opening parameter file '%s'\n", pfname );
+
+		PyErr_SetString( PyExc_RuntimeError, errmsg );
+
+		return NULL;
+	}
+
+	pfvalue = pfget_boolean( pf, pfkey );
+
+	if( pfvalue ) {
+
+		Py_INCREF( Py_True );
+
+		return Py_True;
+
+	} else {
+
+		Py_INCREF( Py_False );
+
+		return Py_False;
+	}
+}
+
+static PyObject *
+python_pfget_time( PyObject *self, PyObject *args ) {
+	char	*usage = "Usage: _pfget_time( pfname, pfkey )\n";
+	char	*pfname;
+	char	*pfkey;
+	double	pfvalue;
+	char	errmsg[STRSZ];
+	Pf	*pf;
+
+	if( ! PyArg_ParseTuple( args, "ss", &pfname, &pfkey ) ) {
+
+		PyErr_SetString( PyExc_RuntimeError, usage );
+
+		return NULL;
+	}
+
+	if( ( pf = getPf( pfname ) ) == (Pf *) NULL ) {
+		
+		sprintf( errmsg, "Failure opening parameter file '%s'\n", pfname );
+
+		PyErr_SetString( PyExc_RuntimeError, errmsg );
+
+		return NULL;
+	}
+
+	pfvalue = pfget_time( pf, pfkey );
+
+	return Py_BuildValue( "d", pfvalue );
+}
 
 static PyObject *
 python_strtime( PyObject *self, PyObject *args ) {
--- data/python/stock/stock.py
+++ data/python/stock/stock.py
@@ -1,14 +1,52 @@
 import _stock
 
+def pfget_string(pfname, pfkey):
+    """Retrieve a string value from a parameter file"""
+
+    return _stock._pfget_string(pfname, pfkey)
+
+
+def pfget_int(pfname, pfkey):
+    """Retrieve an integer value from a parameter file"""
+
+    return _stock._pfget_int(pfname, pfkey)
+
+
+def pfget_double(pfname, pfkey):
+    """Retrieve a double-precision value from a parameter file"""
+
+    return _stock._pfget_double(pfname, pfkey)
+
+
+def pfget_size(pfname, pfkey):
+    """Retrieve a size value from a parameter file"""
+
+    return _stock._pfget_size(pfname, pfkey)
+
+
+def pfget_time(pfname, pfkey):
+    """Retrieve a time value from a parameter file"""
+
+    return _stock._pfget_time(pfname, pfkey)
+
+
+def pfget_boolean(pfname, pfkey):
+    """Retrieve a boolean value from a parameter file"""
+
+    return _stock._pfget_boolean(pfname, pfkey)
+
+
 def strtime(epoch):
     """Convert an epoch time to a standard string"""
 
     return _stock._strtime(epoch)
 
+
 def str2epoch(astring):
     """Convert a string to an epoch time"""
 
     return _stock._str2epoch(astring)
+
 
 if __name__ == '__main__':
     import unittest
@@ -21,6 +59,42 @@ if __name__ == '__main__':
 
         def tearDown(self):
             pass
+
+        def test_pfget_string(self):
+           
+	    val = pfget_string('trdefaults', 'default_trace_schema')
+
+	    self.assertTrue(isinstance(val, str))
+
+        def test_pfget_int(self):
+           
+	    val = pfget_int('trdefaults', 'miniseed_record_size')
+
+	    self.assertTrue(isinstance(val, int))
+
+        def test_pfget_double(self):
+           
+	    val = pfget_double('trdefaults', 'samprate_tolerance')
+
+	    self.assertTrue(isinstance(val, float))
+
+        def test_pfget_size(self):
+           
+	    val = pfget_size('orbserver', 'ringsize')
+
+	    self.assertTrue(isinstance(val, float))
+
+        def test_pfget_time(self):
+           
+	    val = pfget_time('trdefaults', 'pf_revision_time')
+
+	    self.assertTrue(isinstance(val, float))
+
+        def test_pfget_boolean(self):
+           
+	    val = pfget_boolean('trdefaults', 'verbose_splicing')
+
+	    self.assertTrue(isinstance(val, bool))
 
         def test_strtime(self):
 
