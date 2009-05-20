7079eb0ffc1f2a42178a6ce5879bd0ad0b07e654
Kent Lindquist <kent@lindquistconsulting.com>
2009-05-20 19:26:14 +0000

    Add scaffolding for unstuffPkt and antelope.Pkt python wrapper library

---
+++ data/python/Pkt/Pkt.py
@@ -0,0 +1,107 @@
+import _Pkt
+
+from _Pkt import *
+
+class Pkt():
+    """Create an Antelope Packet object
+        
+        Pkt( srcname, time, packet, nbytes )
+    """
+    
+    def __init__(self, *args, **kwargs):
+
+        self._srcname = ''
+	self._time = -9999999999.999
+	self._packet = None
+	self._nbytes = 0
+
+        if(len(args) == 4):
+
+            if(not isinstance(args[0], str)):
+
+                raise TypeError, 'first argument to Pkt() must be a string'
+
+            elif(not isinstance(args[1], float)):
+
+                raise TypeError, 'second argument to Pkt() must be a floating-point value'
+
+            elif(not isinstance(args[2], str)):
+
+                raise TypeError, 'third argument to Pkt() must be a string'
+
+            elif(not isinstance(args[3], int)):
+
+                raise TypeError, 'fourth argument to Pkt() must be an integer'
+
+            else:
+
+                pass
+
+        else:
+
+                raise TypeError, 'Pkt constructor arguments not understood'
+
+
+    def __str__(self):
+        
+        return ("\n[Pkt:\n" +
+            "\tsrcname  = %s\n" % self._srcname +
+            "\ttime  = %s\n" % self._time +
+            "]\n")
+
+    def unstuff(self):
+        """Unpack an Antelope packet"""
+
+        _Pkt._unstuffPkt(srcname, time, packet, nbytes)
+
+def unstuffPkt(srcname, time, packet, nbytes):
+    """Unpack an Antelope packet"""
+
+    return _Pkt._unstuffPkt(srcname, time, packet, nbytes)
+
+
+if __name__ == '__main__':
+    import unittest
+    import os
+    from orb import *
+    orbname = ':dq'
+
+    class TestPkt_fixture():
+
+	tempdir = '/tmp/python_Pkttest_' + os.environ["USER"] + str(os.getpid())
+
+        def start(self):
+
+	    os.mkdir(self.tempdir)
+            os.chdir(self.tempdir)
+            os.system("pfcp orbserver .")
+            os.system("orbserver -p " + orbname + " orbserver &")
+            os.system("sleep 3")
+
+        def stop(self):
+
+            os.system("echo halt | orbstat -i " + orbname)
+            os.system("sleep 5")
+	    os.system("/bin/rm -f " + self.tempdir + "/orb/*")
+	    os.rmdir(self.tempdir + "/orb")
+	    os.system("/bin/rm -f " + self.tempdir + "/*")
+	    os.rmdir(self.tempdir)
+
+    class TestPkt(unittest.TestCase):
+
+        def test_Pkt_constructor(self):
+
+	    self.assertRaises(TypeError, Pkt, 'Pkt constructor arguments not understood')
+	    
+        def test_procedure_unstuffPkt(self):
+
+	    orb = orbopen(orbname, 'r')
+
+            os.system( "pf2orb rtexec " + orbname )
+
+    server = TestPkt_fixture()
+    server.start()
+    suite = unittest.makeSuite(TestPkt)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+    server.stop()
---
+++ data/python/Pkt/_Pkt.c
@@ -0,0 +1,96 @@
+/*
+ *   Copyright (c) 2007-2008 Lindquist Consulting, Inc.
+ *   All rights reserved. 
+ *                                                                     
+ *   Written by Dr. Kent Lindquist, Lindquist Consulting, Inc. 
+ *
+ *   This software is licensed under the New BSD license: 
+ *
+ *   Redistribution and use in source and binary forms,
+ *   with or without modification, are permitted provided
+ *   that the following conditions are met:
+ *   
+ *   * Redistributions of source code must retain the above
+ *   copyright notice, this list of conditions and the
+ *   following disclaimer.
+ *   
+ *   * Redistributions in binary form must reproduce the
+ *   above copyright notice, this list of conditions and
+ *   the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *   
+ *   * Neither the name of Lindquist Consulting, Inc. nor
+ *   the names of its contributors may be used to endorse
+ *   or promote products derived from this software without
+ *   specific prior written permission.
+
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ *   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ *   PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+ *   THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY
+ *   DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ *   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ *   IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ *   USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <stdio.h>
+#include "Python.h"
+#include "Pkt.h"
+
+#ifdef __APPLE__
+
+/* The scaffold below works around a problem on Darwin */
+
+char **environ;
+char *__progname = "Python";
+
+#endif
+
+static PyObject *python_unstuffPkt( PyObject *self, PyObject *args );
+
+PyMODINIT_FUNC init_Pkt( void );
+
+static struct PyMethodDef _Pkt_methods[] = {
+	{ "_unstuffPkt",  	python_unstuffPkt,   	METH_VARARGS, "Unstuff an Antelope orbserver packet" },
+	{ NULL, NULL, 0, NULL }
+};
+
+static PyObject *
+python_unstuffPkt( PyObject *self, PyObject *args ) {
+	char	*usage = "Usage: _unstuffPkt(srcname, time, packet, nbytes)\n";
+	char	srcname[ORBSRCNAME_SIZE];
+	double	pkttime;
+	char	*packet = 0;
+	int	nbytes_pckt = 0;
+	int	nbytes = 0;
+	PyObject *obj;		
+
+	if( ! PyArg_ParseTuple( args, "sds#i", srcname, &pkttime, &packet, &nbytes_pckt, &nbytes) ) {
+
+		PyErr_SetString( PyExc_RuntimeError, usage );
+
+		return NULL;
+	} 
+
+	/* SCAFFOLD */
+
+	obj = Py_BuildValue( "" );
+
+	return obj;
+}
+
+PyMODINIT_FUNC
+init_Pkt( void ) {
+	PyObject *mod;
+
+	mod = Py_InitModule( "_Pkt", _Pkt_methods );
+}
