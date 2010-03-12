192f24de185f1f40599834b0d963681a26cee903
Kent Lindquist <kent@lindquistconsulting.com>
2010-03-12 12:24:02 -0900

    Expand dbmon certify to test all features

--- lib/dataformat/dbmon/certify/try_dbmon.c
+++ lib/dataformat/dbmon/certify/try_dbmon.c
@@ -49,11 +49,18 @@ int
 main(int argc, char **argv )
 {
 	Dbptr	db;
-	char	*dbname = "/opt/antelope/data/db/demo/demo";
+	char	dbname[FILENAME_MAX];
 	Hook	*dbmon_hook = NULL;
-	Tbl	*tables; 
+	Tbl	*tables = (Tbl *) NULL; 
 
-	tables = (Tbl *) NULL;
+	if( argc != 2 ) {
+
+		elog_die( 0, "Usage: %s dbname\n", argv[0] );
+
+	} else {
+		
+		strcpy( dbname, argv[1] );
+	}
 
 	dbopen_database( dbname, "r", &db );
 
@@ -61,5 +68,37 @@ main(int argc, char **argv )
 
 	dbmon_update( dbmon_hook, (void *) stdout );
 
+	fprintf( stdout, "Adding new arrival table:\n" );
+
+	system( "cp data/mod.new.demo.arrival results/dbmon/demo.arrival" );
+
+	sleep( 1 );
+
+	dbmon_update( dbmon_hook, (void *) stdout );
+
+	fprintf( stdout, "Shortening arrival table:\n" );
+
+	system( "cp data/mod.shorter.demo.arrival results/dbmon/demo.arrival" );
+
+	sleep( 1 );
+
+	dbmon_update( dbmon_hook, (void *) stdout );
+
+	fprintf( stdout, "Lengthening arrival table:\n" );
+
+	system( "cp data/mod.longer.demo.arrival results/dbmon/demo.arrival" );
+
+	sleep( 1 );
+
+	dbmon_update( dbmon_hook, (void *) stdout );
+
 	dbmon_status( stdout, dbmon_hook );
+
+	clear_register( 1 );
+
+	free_hook( &dbmon_hook );
+
+	dbclose( db );
+
+	exit( 0 );
 }
