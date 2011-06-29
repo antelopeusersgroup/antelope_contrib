e2b113ff7c3b39aa94677dfea0d08dd31da4867f
Geoff Davis <gadavis@ucsd.edu>
2011-06-29 16:19:20 -0700

    Bump version of dcbba2orb to 1.1.2

--- bin/rt/dcbba2orb/dcbba2orb.c
+++ bin/rt/dcbba2orb/dcbba2orb.c
@@ -50,7 +50,7 @@
 /*
  * Constants
  */
-#define VERSION "dcbba2orb 1.1.1"
+#define VERSION "dcbba2orb 1.1.2"
 /* State variables for use in readFromDC() */
 #define ST_WAIT_FOR_SYNC 0
 #define ST_READ_PKTTYPE 1
