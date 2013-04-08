e4e6f34c8fca61c59443699d6802687d417ebb6b
Mark Williams <mark.c.will@gmail.com>
2013-04-08 11:55:14 -0700

    Added core classes to init

--- data/python/contributed/aug/contrib/orm/__init__.py
+++ data/python/contributed/aug/contrib/orm/__init__.py
@@ -2,8 +2,10 @@
 #
 #
 """
-obspy antelope module
+Object Relational Mapper tools for Antelope Datascope
 """
+from aug.contrib.orm.core import (Dbtuple, Relation, Connection)
 from aug.contrib.orm.dbobjects import (Dbrecord, DbrecordList)
+# Below this line for compatibility with previous versions
 from aug.contrib.orm.dbpointers import (DbrecordPtr, DbrecordPtrList, AttribDbptr)
 from aug.contrib.orm.utils import (add_antelope_path, open_db_or_string)
