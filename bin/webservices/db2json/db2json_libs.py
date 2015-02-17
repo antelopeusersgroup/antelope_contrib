19ecb255cd86770e212b4a9ca8d209e0b63b4a33
Juan Reyes <reyes@ucsd.edu>
2015-02-17 15:57:14 -0800

    Update to db2json methods used by UCSD's rtwebserver instance.

--- bin/webservices/db2json/db2json_libs.py
+++ bin/webservices/db2json/db2json_libs.py
@@ -11,6 +11,9 @@ Safe to use as:
 Juan 12/2014
 """
 
+import os
+import hashlib
+
 
 
 class event2jsonException(Exception):
@@ -36,6 +39,53 @@ class sta2jsonException(Exception):
         return 'sta2jsonException: %s' % (self.msg)
     def __str__(self):
         return repr(self)
+
+
+
+def find_snet(blob,sta,debug=False):
+    """
+    Sometimes we don't know if the snet value of a station.
+    Look in the object for it's snet.
+    """
+
+    for status in blob:
+        for snet in blob[status]:
+            if sta in blob[status][snet]:
+                if debug: print "find_snet(%s) => %s" % (sta,snet)
+                return snet
+
+    if debug: print "find_snet(%s) => False" % sta
+    return False
+
+def find_status(blob,sta,debug=False):
+    """
+    Sometimes we don't know if the station is active or offline.
+    Look in the object for it's status.
+    """
+
+    for status in blob:
+        for snet in blob[status]:
+            if sta in blob[status][snet]:
+                if debug: print "find_status(%s) => %s" % (sta,status)
+                return status
+
+    if debug: print "find_status(%s) => False" % sta
+    return False
+
+def test_yesno(value):
+    """
+    Verify if we have true or false
+    on variable.
+    """
+    try:
+        import antelope.stock as stock
+    except Exception,e:
+        raise sta2jsonException( 'Problems loading Antelope libs: %s' % e )
+
+    try:
+        return stock.yesno( value )
+    except:
+        return bool( value )
 
 
 def test_table(dbname,tbl,verbose=False):
@@ -71,3 +121,35 @@ def test_table(dbname,tbl,verbose=False):
         return False
 
     return path
+
+def get_md5(test_file,debug=False):
+    """
+    Verify the checksum of a table.
+    Return False if no file found.
+    """
+
+    if debug: print 'get_md5(%s) => test for file' % test_file 
+
+    if os.path.isfile( test_file ):
+        f = open(test_file)
+        md5 = hashlib.md5( f.read() ).hexdigest()
+        f.close()
+        return md5
+    else:
+        raise sta2jsonException( "get_md5(%s) => FILE MISSING!!!" % test_file )
+
+
+    return False
+
+def dict_merge(a, b):
+    '''recursively merges dict's. not just simple a['key'] = b['key'], if
+    both a and bhave a key who's value is a dict then dict_merge is called
+    on both values and the result stored in the returned dictionary.'''
+    if not isinstance(b, dict):
+        return b
+    for k, v in b.iteritems():
+        if k in a and isinstance(a[k], dict):
+                a[k] = dict_merge(a[k], v)
+        else:
+            a[k] = v
+    return a
