c6a77ceb6bdd1ba22d390a4fcb1ff56d6b74f854
Nikolaus Horn <horn.niko@gmail.com>
2015-10-31 11:21:26 +0100

    distinguish between rt130A dn rt130B

--- data/python/polygon/_polygon.c
+++ data/python/polygon/_polygon.c
@@ -13,6 +13,7 @@
 
 static PyObject *python_inwhichpolygons( PyObject *self, PyObject *args );
 static PyObject *python_distancetopolygon( PyObject *self, PyObject *args );
+static PyObject *python_readpolygon( PyObject *self, PyObject *args );
 static PyObject *python_windrose( PyObject *self, PyObject *args );
 
 PyMODINIT_FUNC init_polygon( void );
@@ -20,6 +21,7 @@ PyMODINIT_FUNC init_polygon( void );
 static struct PyMethodDef _polygon_methods[] = {
 	{ "_inwhichpolygons",  	python_inwhichpolygons,   METH_VARARGS, "find polygon enclosing point" },
 	{ "_distancetopolygon", python_distancetopolygon, METH_VARARGS, "find minimum distance to polygon" },
+	{ "_readpolygon",       python_distancetopolygon, METH_VARARGS, "read polygon from database" },
 	{ "_windrose",  	    python_windrose,   	      METH_VARARGS, "windrose, 3 chars indicating direction" },
 	{ NULL, NULL, 0, NULL }
 };
@@ -62,6 +64,38 @@ python_distancetopolygon( PyObject *self, PyObject *args ) {
 	P.lon=lon;
 	mindist=distanceToPolygon(db,P);
 	obj =  Py_BuildValue( "d", mindist );
+	return obj;
+}
+static PyObject *
+python_readpolygon( PyObject *self, PyObject *args ) {
+	char    *usage = "Usage: dbout= _polygon._readpolygon( dbin )\n";
+	Dbptr	db;
+	Point 	*poly;
+	double  lat,lon;
+    char    pname[1024];
+    long    npolygons, nvertices;
+	PyObject *obj;
+	
+	if( ! PyArg_ParseTuple( args, "O&",parse_to_Dbptr,&db ) ) {
+
+		USAGE;
+
+		return NULL;
+	}
+    if (db.table < 0) {
+        db=dblookup(db, 0, "polygon", 0, 0);
+    }
+    dbquery(db,dbRECORD_COUNT, &npolygons);
+    for (db.record=0; db.record < npolygons; db.record++) {
+        nvertices=readPolygon(db, &poly);
+        dbgetv(db, 0, "pname", &pname); 
+        for (int i=0; i< nvertices; i++) {
+            printf("%.4f %.4f\n", poly[i].lon, poly[i].lat);
+        }
+        free(poly);
+    }
+
+	obj =  Py_BuildValue( "l", nvertices );
 	return obj;
 }
 static PyObject *
