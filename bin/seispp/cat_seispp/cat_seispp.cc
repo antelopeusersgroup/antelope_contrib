4f5acedaa680c129a75fdb1d16efe737ebb6a494
Gary Pavlis <pavlis@indiana.edu>
2017-05-27 06:57:29 -0400

    New seispp module to concenate files

---
+++ bin/seispp/cat_seispp/cat_seispp.cc
@@ -0,0 +1,127 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <memory>
+#include <list>
+#include "seispp.h"
+#include "ensemble.h"
+#include "StreamObjectReader.h"
+#include "StreamObjectWriter.h"
+#include "PMTimeSeries.h"
+using namespace std;   
+using namespace SEISPP; 
+void usage()
+{
+    cerr << "cat file1 file2 ... filen [-binary --help]"
+        <<endl
+        << "Concatenate a set of seispp files into a single larger file"<<endl
+        << " --help - prints this message"<<endl
+        << " -binary - switch to binary input and output (default is text)"
+        << "(Note:  will exit with an error if the count of files is only one)"
+        <<endl;
+    exit(-1);
+}
+/* This was copied from listhdr - should probably become a compiled
+   procedure */
+enum AllowedObjects {TCS, TCE, PMTS, TS, TSE};
+AllowedObjects get_object_type(string otype)
+{
+    if(otype=="ThreeComponentSeismogram")
+        return TCS;
+    else if(otype=="ThreeComponentEnsemble")
+        return TCE;
+    else if(otype=="PMTimeSeries")
+        return PMTS;
+    else
+    {
+        cerr << "Do not know how to handle object type="<<otype
+            <<endl<< "Cannot continue"<<endl;
+        exit(-1);
+    }
+}
+template <typename T> 
+     void cat_seispp_generic(list<string>& files,bool binary_data)
+{
+  char dform;
+  if(binary_data)
+    dform='b';
+  else
+    dform='t';
+  try{
+    /* Output is always to stdout */
+    StreamObjectWriter<T> ofs;
+    list<string>::iterator fptr;
+    for(fptr=files.begin();fptr!=files.end();++fptr)
+    {
+      StreamObjectReader<T> ifs(fptr->c_str(),dform);
+      T d;
+      while(ifs.good())
+      {
+        d=ifs.read();
+        ofs.write(d);
+      }
+    }
+  }catch(...){throw;};
+}
+bool SEISPP::SEISPP_verbose(true);
+int main(int argc, char **argv)
+{
+    int i;
+    bool binary_data(false);
+    list<string> FileList;
+    AllowedObjects otype;
+    if(argc<=2) usage();
+
+    for(i=1;i<argc;++i)
+    {
+        string sarg(argv[i]);
+        if(sarg=="--help")
+        {
+            usage();
+        }
+        else if(sarg=="-binary")
+        {
+            binary_data=true;
+        }
+        else if(sarg=="-t")
+        {
+          ++i;
+          if(i>argc) usage();
+          otype=get_object_type(string(argv[i]));
+        }
+        else
+        {
+          FileList.push_back(sarg);
+        }
+    }
+    if(FileList.size()<2) usage();
+    try{
+      switch(otype)
+      {
+        case TCS:
+          cat_seispp_generic<ThreeComponentSeismogram>(FileList,binary_data);
+          break;
+        case TS:
+          cat_seispp_generic<TimeSeries>(FileList,binary_data);
+          break;
+        case TSE:
+          cat_seispp_generic<TimeSeriesEnsemble>(FileList,binary_data);
+          break;
+        case PMTS:
+          cat_seispp_generic<PMTimeSeries>(FileList,binary_data);
+          break;
+        case TCE:
+        default:
+          cat_seispp_generic<ThreeComponentEnsemble>(FileList,binary_data);
+      };
+    }catch(SeisppError& serr)
+    {
+        serr.log_error();
+    }
+    catch(std::exception& stexc)
+    {
+        cerr << stexc.what()<<endl;
+    }
+}
+
