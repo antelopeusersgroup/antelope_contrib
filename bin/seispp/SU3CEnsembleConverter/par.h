c625ea1c488a022aaf623dfd3d3c1fe910bbc068
Gary Pavlis <pavlis@indiana.edu>
2017-09-22 05:24:13 -0400

    Moved this program from private work area after conversion to new write new stream file format

---
+++ bin/seispp/SU3CEnsembleConverter/par.h
@@ -0,0 +1,16 @@
+/* Copyright (c) Colorado School of Mines, 2007.*/
+/* All rights reserved.                       */
+
+/* par.h - include file for getpar, selfdoc, and error handling functions */
+
+#ifndef PAR_H
+#define PAR_H
+
+
+/* INCLUDES */
+
+//#include "cwp.h"
+
+/* GLOBAL DECLARATIONS */
+extern int xargc; extern char **xargv;
+#endif /* PAR_H */
