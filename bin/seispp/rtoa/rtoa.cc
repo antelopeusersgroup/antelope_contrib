0717202dc8005ed0381e3fc44ea27aaf910f5e23
pavlis <pavlis@indiana.edu>
2017-11-12 15:00:52 -0500

    New program - inverse of existing ator program

---
+++ bin/seispp/rtoa/rtoa.cc
@@ -0,0 +1,119 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <memory>
+#include "seispp.h"
+#include "ensemble.h"
+#include "StreamObjectReader.h"
+#include "StreamObjectWriter.h"
+using namespace std;
+using namespace SEISPP;
+void usage()
+{
+    cerr << "rtoa < in > out [-t objt --help -text -v]"
+        <<endl
+        << "Switches input data from absolute to a relative time standard"<<endl
+        <<endl
+        << "Use -objt to change expected object type"<<endl
+        << "(default ThreeComponentSeismogram.  Alteratives are TimeSeries and PMTimeSeries)"<<endl
+        << " --help - prints this message"<<endl
+        << " -text - switch to text input and output (default is binary)"
+        << " -v - be more verbose"<<endl
+        <<endl;
+    exit(-1);
+}
+enum AllowedObjects {TCS, PMTS, TS};
+AllowedObjects get_object_type(string otype)
+{
+    if(otype=="ThreeComponentSeismogram")
+        return TCS;
+    else if(otype=="TimeSeries")
+        return TS;
+    else if(otype=="PMTimeSeries")
+        return PMTS;
+    else
+    {
+        cerr << "rtoa: cannot handle object type="<<otype
+            <<endl<< "Cannot continue"<<endl;
+        exit(-1);
+    }
+}
+/* Returns number of objects processed.   Read from stdin and write
+ * to stdout defined object type.   */
+template <typename T> int rtoa(bool binary_data)
+{
+  try{
+    char form('t');
+    if(binary_data)form='b';
+    StreamObjectReader<T> inp(form);
+    StreamObjectWriter<T> outp(form);
+    int dcount(0);
+    while(inp.good())
+    {
+      T d=inp.read();
+      d.rtoa();
+      outp.write(d);
+      ++dcount;
+    }
+    return dcount;
+  }catch(...){throw;};
+}
+bool SEISPP::SEISPP_verbose(false);
+int main(int argc, char **argv)
+{
+    int i;
+    AllowedObjects objt(TCS);
+    if(argc<=1) usage();
+    if(string(argv[1])=="--help") usage();
+    bool binary_data(true);
+
+    for(i=1;i<argc;++i)
+    {
+        string sarg(argv[i]);
+        if(sarg=="--help")
+        {
+            usage();
+        }
+        else if(sarg=="-text")
+        {
+            binary_data=false;
+        }
+        else if(sarg=="-v")
+        {
+            SEISPP_verbose=true;
+        }
+        else if(sarg=="-t")
+        {
+            ++i;
+            if(i>=argc) usage();
+            objt=get_object_type(string(argv[i]));
+        }
+        else
+            usage();
+    }
+    try{
+      int nprocessed;
+      switch(objt)
+      {
+      case PMTS:
+        nprocessed=rtoa<ThreeComponentSeismogram>(binary_data);
+        break;
+      case TCS:
+        nprocessed=rtoa<ThreeComponentSeismogram>(binary_data);
+        break;
+      case TS:
+      default:
+        nprocessed=rtoa<ThreeComponentSeismogram>(binary_data);
+      };
+      if(SEISPP_verbose) cerr << "rtoa:  processed "<<nprocessed
+          << " objects from stdin"<<endl;
+    }catch(SeisppError& serr)
+    {
+        serr.log_error();
+    }
+    catch(std::exception& stexc)
+    {
+        cerr << stexc.what()<<endl;
+    }
+}
