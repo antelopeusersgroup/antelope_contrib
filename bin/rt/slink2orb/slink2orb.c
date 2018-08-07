4b799c83b182a3c81f86836dbddbfcef583b4687
Chad Trabant <chad.trabant@iris.edu>
2018-08-07 16:37:46 -0700

    Code formatting cleanup

--- bin/rt/slink2orb/slink2orb.c
+++ bin/rt/slink2orb/slink2orb.c
@@ -3,8 +3,6 @@
  * A SeedLink to Antelope ORB module
  *
  * Written by Chad Trabant, ORFEUS/EC-Project MEREDIAN
- *
- * modified: 2015.187
  ***************************************************************************/
 
 #include <signal.h>
@@ -53,8 +51,8 @@ main(int argc, char **argv)
   SLpacket *slpack;
   int seqnum;
   int ptype;
-  int packetcnt = 0;  
-      
+  int packetcnt = 0;
+
   /* Signal handling, use POSIX calls with standardized semantics */
   struct sigaction sa;
 
@@ -62,12 +60,12 @@ main(int argc, char **argv)
   sa.sa_flags = SA_RESTART;
   sigemptyset(&sa.sa_mask);
   sigaction(SIGALRM, &sa, NULL);
-  
+
   sa.sa_handler = term_handler;
   sigaction(SIGINT, &sa, NULL);
   sigaction(SIGQUIT, &sa, NULL);
   sigaction(SIGTERM, &sa, NULL);
-  
+
   sa.sa_handler = SIG_IGN;
   sigaction(SIGHUP, &sa, NULL);
   sigaction(SIGPIPE, &sa, NULL);
@@ -104,7 +102,7 @@ main(int argc, char **argv)
 	}
       finit_db(dbase);
     }
-  
+
   /* Loop with the connection manager */
   while ( sl_collect (slconn, &slpack) )
     {
@@ -130,16 +128,16 @@ main(int argc, char **argv)
   /* Shutdown */
   if (slconn->link != -1)
     sl_disconnect (slconn);
-  
+
   if (orb != -1)
     orbclose(orb);
-  
+
   if (statefile)
     sl_savestate (slconn, statefile);
-  
+
   /* flush the error log just in case */
   elog_print(stdout, 0);
-  
+
   return 0;
 }				/* End of main() */
 
@@ -158,14 +156,14 @@ packet_handler (char *msrecord, int packet_type, int seqnum, int packet_size)
   int         nbytes = 0;
 
   char srcname[ORBSRCNAME_SIZE];
-  
+
   /* Process regular miniSEED records and send them on */
   if ( packet_type >= SLDATA && packet_type <= SLNUM )
     {
       mseedret = mseed2orbpkt(msrecord, packet_size, calibdb, mappingdb,
 			      remap, srcname, &time, &packet, &nbytes,
 			      &bufsize);
-      
+
       if ( verbose >= 3)
 	{
 	  char *s = strydtime(time);
@@ -173,9 +171,9 @@ packet_handler (char *msrecord, int packet_type, int seqnum, int packet_size)
 		  srcname, s, seqnum);
 	  free(s);
 	}
-      
+
       if (mseedret == 0)
-	{ 
+	{
 	  if (orbput(orb, srcname, time, packet, nbytes))
 	    sl_log(1, 0, "orbput() failed: %s(%d)\n", srcname, nbytes);
 	}
@@ -208,7 +206,7 @@ parameter_proc(int argcount, char **argvec)
   char netto_argv = 0;
   char netdly_argv = 0;
   char keepalive_argv = 0;
-  
+
   if (argcount <= 2)
     usage();
 
@@ -267,13 +265,13 @@ parameter_proc(int argcount, char **argvec)
 
   /* Initialize the verbosity for the libslink functions */
   sl_loginit (verbose, &elog_printlog, NULL, &elog_printerr, NULL);
-  
+
   sl_log (0,0, "%s version %s\n", package, version);
 
   /* For the last two required arguments */
   if ((argcount - optind) < 2)
     usage();
-  
+
   for (; optind < argcount; optind++)
     {
       if (optind < (argcount - 2))
@@ -293,33 +291,33 @@ parameter_proc(int argcount, char **argvec)
   else
     {
       /* Do some extra work so parameters are optional */
-      
+
       /* Only read network timeout if not set on command line */
       if (!netto_argv)
 	if ((tptr = pfget_string(pf, "nettimeout")) != 0)
 	  slconn->netto = atoi((char *) tptr);
-      
+
       /* Only read network reconnect delay if not set on command line */
       if (!netdly_argv)
 	if ((tptr = pfget_string(pf, "netdelay")) != 0)
 	  slconn->netdly = atoi((char *) tptr);
-      
+
       /* Only read keepalive interval if not set on command line */
       if (!keepalive_argv)
 	if ((tptr = pfget_string(pf, "keepalive")) != 0)
 	  slconn->keepalive = atoi((char *) tptr);
-      
+
       if ((tptr = pfget_string(pf, "stateint")) != 0)
 	stateint = atoi((char *) tptr);
-      
+
       if ((tptr = pfget_string(pf, "selectors")) != 0 &&
 	  strlen(tptr) > 0)
 	selectors = strdup((char *) tptr);
-      
+
       /* 'stations' == 0 if not found */
       stations = pfget_arr(pf, "stations");
     }
-  
+
   /* Translate the 'stations' Arr, if given */
   if (stations != 0)
     {
@@ -344,7 +342,7 @@ parameter_proc(int argcount, char **argvec)
 	      return -1;
 	    }
 	  *stap++ = '\0';
-	  
+
 	  /* Check the net/sta pair */
 	  if (strlen(netp) > 5 || strlen(netp) <= 0)
 	    {
@@ -356,7 +354,7 @@ parameter_proc(int argcount, char **argvec)
 	      sl_log(1, 0, "Problem with station code: %s\n", key);
 	      return -1;
 	    }
-	  
+
 	  /* Retreive selectors for this station, if none use default */
 	  tptr = (char *) getarr(stations, key);
 	  if (strlen(tptr) == 0)
@@ -367,10 +365,10 @@ parameter_proc(int argcount, char **argvec)
 	    {
 	      selp = tptr;
 	    }
-	  
+
 	  /* Add this to the list */
 	  sl_addstream (slconn, netp, stap, selp, -1, NULL);
-	  
+
 	  stacount++;
 	  free(netp);
 	}
@@ -398,7 +396,7 @@ parameter_proc(int argcount, char **argvec)
 	  sl_log (1, 0, "state recovery failed\n");
 	}
     }
-  
+
   return 0;
 }				/* End of parameter_proc() */
 
@@ -412,19 +410,19 @@ report_environ()
 {
   int         stacount;
   SLstream   *curstation;
-  
+
   if (statefile)
     sl_log(0, 0, "statefile:\t%s\n", statefile);
   else
     sl_log(0, 0, "'statefile' not defined\n");
-  
+
   sl_log(0, 0, "stateint:\t%d\n", stateint);
-  
+
   if (paramfile)
     sl_log(0, 0, "paramfile:\t%s\n", paramfile);
   else
     sl_log(0, 0, "'paramfile' not defined\n");
-  
+
   if (mappingdb)
     sl_log(0, 0, "mappingdb:\t%s\n", mappingdb);
   else
@@ -434,7 +432,7 @@ report_environ()
     sl_log(0, 0, "calibdb:\t%s\n", calibdb);
   else
     sl_log(0, 0, "'calibdb' not defined\n");
-  
+
   sl_log(0, 0, "verbose:\t%d\n", verbose);
   sl_log(0, 0, "remap:\t%d\n", remap);
   sl_log(0, 0, "nettimeout:\t%d\n", slconn->netto);
@@ -445,24 +443,24 @@ report_environ()
     sl_log(0, 0, "selectors:\t%s\n", selectors);
   else
     sl_log(0, 0, "'selectors' not defined\n");
-    
+
   if (orbaddr)
     sl_log(0, 0, "orbaddr:\t%s\n", orbaddr);
   else
     sl_log(0, 0, "'orbaddr' not defined\n");
-  
+
   if (slconn->sladdr)
     sl_log(0, 0, "sladdr:\t%s\n", slconn->sladdr);
   else
     sl_log(0, 0, "'slconn->sladdr' not defined\n");
 
   sl_log(0, 0, "link:\t%d\n", slconn->link);
-  
+
   sl_log(0, 0, "slconn->multistation:\t%d\n", slconn->multistation);
-  
+
   stacount = 0;
   curstation = slconn->streams;
-  
+
   sl_log(0, 0, "'stations' array:\n");
   while (curstation != NULL)
     {
@@ -471,22 +469,22 @@ report_environ()
 	       stacount, curstation->net);
       else
 	sl_log(0, 0, "'net' not defined\n");
-      
+
       if (curstation->sta)
 	sl_log(0, 0, "  %d - sta: %s\n",
 	       stacount, curstation->sta);
       else
 	sl_log(0, 0, "'sta' not defined\n");
-      
+
       if (curstation->selectors)
 	sl_log(0, 0, "  %d - selectors: %s\n",
 	       stacount, curstation->selectors);
       else
 	sl_log(0, 0, "  %d - 'selectors' not defined\n", stacount);
-      
+
       sl_log(0, 0, "  %d - seqnum: %d\n", stacount, curstation->seqnum);
       sl_log(0, 0, "  %d - timestamp: '%s'\n", stacount, curstation->timestamp);
-      
+
       stacount++;
       curstation = curstation->next;
     }
@@ -552,6 +550,6 @@ usage(void)
 	 "\n"
 	 "Please report problems to chad@iris.washington.edu\n"
 	 "\n");
-  
+
   exit(1);
 }
