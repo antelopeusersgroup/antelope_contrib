e5ac02a9cb4bb3bd440e54806796d76deab2aa4b
pavlis <pavlis@indiana.edu>
2018-10-09 13:09:17 -0400

    Working version on linux with matlab 2018a

--- bin/seispp/mtspec/MTSpectrum.cc
+++ bin/seispp/mtspec/MTSpectrum.cc
@@ -2,7 +2,6 @@
 #include <vector>
 #include "TimeSeries.h"
 #include "ThreeComponentSeismogram.h"
-#include "Ensemble.h"
 #include "MTSpectrum.h"
 using namespace std;
 using namespace SEISPP;
@@ -42,19 +41,36 @@ matlab to compute multitaper spectrum.  Args are
 
 dname - matlab symbol name for data vector or matrix
 tbp - time bandwidth product.
+N - length of array sent to matlab
+fs - sampling frequncy in Hz
 */
-string build_process_commands(string dname, distinction tbp, int N, double fs)
+string build_process_commands(string dname, double tbp, int N, double fs)
 {
   stringstream ss;
   ss << "["<<specname <<",f]"
     << "=pmtm("<<dname<<","<<tbp<<","<<N<<","<<fs<<")"<<endl;
+  //DEBUG
+  cerr << "Process script sent to matlab"<<endl
+      << ss.str()<<endl;
   return(ss.str());
 }
-TimeSeries MTSpectrum::spectrum(Metadata& md, double d, int nd, double fs)
+TimeSeries MTSpectrum::spectrum(Metadata& md, double *d, int nd)
 {
   try{
     string commands;
-    commands=build_process_commands(vname,this->tbp);
+    double fs;
+    /* Attempt to extract sampling frequency from Metadata.  If
+     * if tails default to 1.0 */
+    try{
+        fs=md.get<double>("samprate");
+        fs=1.0/fs;
+    }catch(MetadataGetError& mde)
+    {
+        cerr << "Warning(MTSpectrum::spectrum(Metadata& md, double *d, int nd) method"<<endl
+            << "samprate attribute not defined - defaulting to 1.0. "<<endl
+            << "Computed frequencies will likely be wrong"<<endl;
+    }
+    commands=build_process_commands(vname,this->tbp,nd,fs);
     matlab->load(d,nd,vname);
     matlab->process(commands);
     vector<double> s,f;
@@ -66,7 +82,7 @@ TimeSeries MTSpectrum::spectrum(Metadata& md, double d, int nd, double fs)
     dts.ns=s.size();
     dts.dt=df;
     dts.t0=0.0;
-    dts.tref=Relative;
+    dts.tref=relative;
     dts.live=true;
     return dts;
   }catch(...){throw;};
@@ -74,14 +90,16 @@ TimeSeries MTSpectrum::spectrum(Metadata& md, double d, int nd, double fs)
 TimeSeries MTSpectrum::spectrum(Metadata& md, vector<double>& d)
 {
   try{
-    return(this->matlab->spectrum(md,&(d[0]),d.size()));
+    return(this->spectrum(md,&(d[0]),d.size()));
   }catch(...){throw;};
 }
 TimeSeries MTSpectrum::spectrum(TimeSeries d)
 {
   try{
     string commands;
-    commands=build_process_commands(vname,this->tbp);
+    double fs;
+    fs=1.0/(d.dt);  
+    commands=build_process_commands(vname,this->tbp,d.s.size(),fs);
     /* There is an overloaded method to directoy load a TimeSeries data
     vector as vname */
     matlab->load(d,vname);
@@ -95,7 +113,7 @@ TimeSeries MTSpectrum::spectrum(TimeSeries d)
     d.ns=s.size();
     d.dt=df;
     d.t0=0.0;
-    d.tref=Relative;
+    d.tref=relative;
     d.live=true;
     return d;
   }catch(...){throw;};
@@ -104,11 +122,13 @@ ThreeComponentSeismogram MTSpectrum::spectrum(ThreeComponentSeismogram d)
 {
   try{
     string commands;
-    commands=build_process_commands(mname,this->tbp);
+    double fs;
+    fs=1.0/(d.dt);  
+    commands=build_process_commands(mname,this->tbp,d.ns,fs);
     /* Matlab requires the component data to be in the columns, but the
     3c object stores sample data in the columns. */
-    dmatrix ut=SEISPP::tr(d.u);
-    matlab->load(d,mname);
+    dmatrix ut=tr(d.u);
+    matlab->load(ut,mname);
     matlab->process(commands);
     shared_ptr<dmatrix> u;
     vector<double> f;
@@ -116,12 +136,40 @@ ThreeComponentSeismogram MTSpectrum::spectrum(ThreeComponentSeismogram d)
     f=matlab->retrieve_vector(freqname);
     double df=f[1]-f[0];
     /* We modify the copy of d to hold the power spectrum estimate*/
-    d.u=SEISPP::tr(u);
-    d.ns=u.rows();
+    ut=tr(*u);
+    d.u=ut;
+    d.ns=d.u.columns();
     d.dt=df;
     d.t0=0.0;
-    d.tref=Relative;
+    d.tref=relative;
     d.live=true;
+    return d;
+  }catch(...){throw;};
+}
+
+TimeSeriesEnsemble MTSpectrum::spectrum(TimeSeriesEnsemble d)
+{
+  try{
+    int i;
+    for(i=0;i<d.member.size();++i)
+    {
+      TimeSeries spec;
+      spec=this->spectrum(d.member[i]);
+      d.member[i]=spec;
+    }
+    return d;
+  }catch(...){throw;};
+}
+ThreeComponentEnsemble MTSpectrum::spectrum(ThreeComponentEnsemble d)
+{
+  try{
+    int i;
+    for(i=0;i<d.member.size();++i)
+    {
+      ThreeComponentSeismogram spec;
+      spec=this->spectrum(d.member[i]);
+      d.member[i]=spec;
+    }
     return d;
   }catch(...){throw;};
 }
