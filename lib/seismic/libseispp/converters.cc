54a4650391cf9542fd06afa1401a8b4795d6541c
Gary Pavlis <pavlis@indiana.edu>
2019-01-20 09:09:48 -0500

    Changes to match interface change for TimeSeries

--- lib/seismic/libseispp/converters.cc
+++ lib/seismic/libseispp/converters.cc
@@ -10,15 +10,11 @@ simply rethrown.
 This version clones the entire metadata space of the parent to the 
 output TimeSeries.  
 */
-TimeSeries *ExtractComponent(ThreeComponentSeismogram& tcs,int component)
+TimeSeries *ExtractComponent(const ThreeComponentSeismogram& tcs,const int component)
 {
     try {
-	TimeSeries *ts=new TimeSeries(dynamic_cast<Metadata&>(tcs),false);
-        /* This absurd set of gyrations is needed to copy the BasicTimeSeries components of
-         * the parent */
-        BasicTimeSeries *bts=dynamic_cast<BasicTimeSeries*>(ts);
-        BasicTimeSeries *pbts=dynamic_cast<BasicTimeSeries*>(&tcs);
-        *bts=bts->BasicTimeSeries::operator=(*pbts);
+	TimeSeries *ts=new TimeSeries(dynamic_cast<const BasicTimeSeries&>(tcs),
+              dynamic_cast<const Metadata&>(tcs));
 	if(ts->live)
 	    for(int i=0;i<tcs.ns;++i) 
 		ts->s.push_back(tcs.u(component,i));
@@ -30,18 +26,15 @@ TimeSeries *ExtractComponent(ThreeComponentSeismogram& tcs,int component)
     }
 }
 // Overloaded version to do a selective copy
-TimeSeries *ExtractComponent(ThreeComponentSeismogram& tcs,int component,
-	MetadataList& mdl)
+TimeSeries *ExtractComponent(const ThreeComponentSeismogram& tcs,const int component,
+	const MetadataList& mdl)
 {
     try {
 	Metadata mdclone;
-	copy_selected_metadata(dynamic_cast<Metadata &>(tcs),
+	copy_selected_metadata(dynamic_cast<const Metadata &>(tcs),
 		mdclone,mdl);
-	TimeSeries *ts=new TimeSeries(mdclone,false);
-	// Same crazy gyrations as above to copy BasicTimeSeries attributes
-        BasicTimeSeries *bts=dynamic_cast<BasicTimeSeries*>(ts);
-        BasicTimeSeries *pbts=dynamic_cast<BasicTimeSeries*>(&tcs);
-        *bts=bts->BasicTimeSeries::operator=(*pbts);
+	TimeSeries *ts=new TimeSeries(dynamic_cast<const BasicTimeSeries&>(tcs),
+            mdclone);
 	for(int i=0;i<tcs.ns;++i) 
 		ts->s.push_back(tcs.u(component,i));
 	return(ts);
